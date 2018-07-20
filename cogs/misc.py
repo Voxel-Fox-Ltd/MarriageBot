58190f8142ce0783ae119eb23a772fc29ec2d26f
Caleb Bartlett <coolum444@hotmail.co.uk>
2018-07-20 14:27:31 +0100

    Add info commands

---
+++ cogs/misc.py
@@ -0,0 +1,42 @@
+from discord.ext.commands import command, Context
+from cogs.utils.custom_bot import CustomBot 
+
+
+class Misc(object):
+
+    def __init__(self, bot:CustomBot):
+        self.bot = bot 
+
+
+    @command(aliases=['git', 'code'])
+    async def github(self, ctx:Context):
+        '''
+        Gives you a link to the bot's code repository
+        '''
+
+        await ctx.send(f"<{self.bot.config['github']}>")
+
+
+    @command()
+    async def patreon(self, ctx:Context):
+        '''
+        Gives you the creator's Patreon link
+        '''
+
+        await ctx.send(f"<{self.bot.config['patreon']}>")
+
+
+    @command()
+    async def invite(self, ctx:Context):
+        '''
+        Gives you an invite link for the bot
+        '''
+
+        await ctx.send(
+            f"<https://discordapp.com/oauth2/authorize?client_id={self.bot.user.id}&permissions=3072>"
+        )
+
+
+def setup(bot:CustomBot):
+    x = Misc(bot)
+    bot.add_cog(x)
