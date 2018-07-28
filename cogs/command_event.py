1a288528709dac819d6c221515d2e17fd9d93f76
Caleb Bartlett <coolum444@hotmail.co.uk>
2018-07-28 06:17:35 +0100

    Fix valid-command logging

--- cogs/command_event.py
+++ cogs/command_event.py
@@ -1,3 +1,4 @@
+from discord import Message
 from discord.ext.commands import Context
 from cogs.utils.custom_bot import CustomBot
 
@@ -15,21 +16,19 @@ class CommandEvent(object):
         return channel  
 
 
-    async def print_message(self, ctx:Context):
-        await self.log_channel.send(f"Guild: `{ctx.guild.name}` (`{ctx.guild.id}`) | User: `{ctx.author!s}` (`{ctx.author.id}`)\nContent: `{ctx.message.content}`")
-
-
     async def on_command(self, ctx:Context):
         '''
         Runs when a command is run
         '''
 
-        await self.print_message(ctx)
+        await self.log_channel.send(f"Guild: `{ctx.guild.name}` (`{ctx.guild.id}`) | User: `{ctx.author!s}` (`{ctx.author.id}`)\nContent: `{ctx.message.content}`")
 
 
-    async def on_message(self, ctx:Context):
-        if any([i in ctx.content.casefold() for i in ['marriagebot', 'marriage bot', f'{self.bot.user.id}']]):
-            await self.print_message(ctx)
+    async def on_message(self, message:Message):
+        if message.author.bot:
+            return
+        if any([i in message.content.casefold() for i in ['marriagebot', 'marriage bot', f'{self.bot.user.id}']]):
+            await self.log_channel.send(f"Guild: `{message.guild.name}` (`{message.guild.id}`) | User: `{message.author!s}` (`{message.author.id}`)\nContent: `{message.content}`")
 
 
 def setup(bot:CustomBot):
