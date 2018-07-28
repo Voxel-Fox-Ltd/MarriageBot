2882364d969fc88e463081d690c349eb987ab660
Caleb Bartlett <coolum444@hotmail.co.uk>
2018-07-28 05:01:21 +0100

    Add error checking for forbidden Lots of people didn't realise they needed to give the bot "send messages" permissions.

--- cogs/error_event.py
+++ cogs/error_event.py
@@ -1,6 +1,6 @@
 from traceback import format_exc
 from discord.ext.commands import Context
-from discord.ext.commands import MissingRequiredArgument, BadArgument, CommandNotFound, CheckFailure
+from discord.ext.commands import MissingRequiredArgument, BadArgument, CommandNotFound, CheckFailure, CommandInvokeError
 from cogs.utils.custom_bot import CustomBot
 from cogs.utils.checks.can_send_files import CantSendFiles
 
@@ -18,6 +18,14 @@ class ErrorEvent(object):
         return channel   
 
 
+    async def on_command(self, ctx:Context):
+        '''
+        Runs when a command is run
+        '''
+
+        await self.log_channel.send(f"Guild: `{ctx.guild.name}` (`{ctx.guild.id}`) | User: `{ctx.author!s}` (`{ctx.author.id}`)\nContent: `{ctx.message.content}`")
+
+
     async def on_command_error(self, ctx:Context, error):
         '''
         Runs when there's an error thrown somewhere in the bot
@@ -32,6 +40,9 @@ class ErrorEvent(object):
         elif isinstance(error, CantSendFiles):
             await ctx.send("I'm not able to send files into this channel.")
             return
+        elif isinstance(error, CommandInvokeError):
+            await ctx.author.send("I'm unable to send messages in that channel.")
+            return
         elif isinstance(error, CheckFailure):
             # The only checks are the CalebOnly commands
             return
@@ -43,7 +54,7 @@ class ErrorEvent(object):
                 raise error 
             except Exception as e:
                 await self.log_channel.send(
-                    f"**Error**\nUser: `{ctx.author}` (`{ctx.author.id}`) | Guild: `{ctx.guild.id}` | Content: `{ctx.message.content}`\n" + 
+                    f"**Error**\nUser: `{ctx.author!s}` (`{ctx.author.id}`) | Guild: `{ctx.guild.name}` (`{ctx.guild.id}`) | Content: `{ctx.message.content}`\n" + 
                     "```py\n" + format_exc() + "```"
                     )
 
