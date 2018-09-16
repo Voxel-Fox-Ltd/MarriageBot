6cefc633f994598b445f57b094ce1f6f0cbba5e7
Caleb Bartlett <coolum444@hotmail.co.uk>
2018-09-16 02:53:36 +0100

    Add colours by name in customisation

--- cogs/customisation.py
+++ cogs/customisation.py
@@ -1,6 +1,7 @@
 from discord.ext.commands import command, group, Context
 from cogs.utils.custom_bot import CustomBot
 from cogs.utils.customised_tree_user import CustomisedTreeUser
+from cogs.utils.colour_dict import COLOURS
 
 
 class Customisation(object):
@@ -10,6 +11,52 @@ class Customisation(object):
 
     def __init__(self, bot:CustomBot):
         self.bot = bot
+
+
+    async def coloursetter(self, ctx:Context, colour:str, attribute:str):
+        '''
+        Actually does all the heavy lifting for the colour setters
+        '''
+
+        tree = CustomisedTreeUser.get(ctx.author.id)
+        if colour == None:
+            if getattr(tree, attribute) == None:
+                await ctx.send("You already have that set to the default colour.")
+            else:
+                setattr(tree, attribute, None)
+                await ctx.send("Set to the default colour.")
+            async with self.bot.database() as db:
+                try:
+                    await db(f'UPDATE customisation SET {attribute}=null WHERE user_id=$1', ctx.author.id)
+                except Exception:
+                    pass
+            return 
+
+        # Run through checks and make sure it's a real colour
+        colour = colour.strip('#.')
+        colour_by_name = COLOURS.get(colour.lower())
+        if colour_by_name != None:
+            hex_colour = colour_by_name
+        else:
+            if len(colour) != 6:
+                await ctx.send("The colour you passed is not a valid colour name or hex code.") 
+                return
+            try:
+                hex_colour = int(colour, 16)
+            except ValueError:
+                await ctx.send("The colour you passed is not a valid colour name or hex code.") 
+                return
+
+        # Valid colour, save
+        async with self.bot.database() as db:
+            try:
+                await db(f'INSERT INTO customisation (user_id, {attribute}) VALUES ($1. $2)', ctx.author.id, hex_colour)
+            except Exception:
+                await db(f'UPDATE customisation SET {attribute}=$1 WHERE user_id=$2', hex_colour, ctx.author.id)
+
+        setattr(tree, attribute, hex_colour)
+        await ctx.send("Customisation saved.") 
+        CustomisedTreeUser.all_users[ctx.author.id] = tree
 
 
     @group(aliases=['customize'])
@@ -27,230 +74,57 @@ class Customisation(object):
 
 
     @customise.command(aliases=['line', 'lines', 'edges'])
-    async def edge(self, ctx:Context, colour:str=None):
+    async def edge(self, ctx:Context, *, colour:str=None):
         '''
         Changes the colour of the lines linking users
         '''
         
-        tree = CustomisedTreeUser.get(ctx.author.id)
-        if colour == None:
-            if tree.edge == None:
-                await ctx.send("You already have that set to the default colour.")
-                return 
-            tree.edge = None 
-            await ctx.send("Set to the default colour.")
-            return 
-
-        # Run through checks and make sure it's a real colour
-        colour = colour.strip('#')
-        if len(colour) != 6:
-            await ctx.send("This command only accepts a valid hex code.") 
-            return
-        try:
-            hex_colour = int(colour, 16)
-        except ValueError:
-            await ctx.send("This command only accepts a valid hex code.") 
-            return
-
-        # Valid colour, save
-        async with self.bot.database() as db:
-            try:
-                await db('INSERT INTO customisation (user_id) VALUES ($1)', ctx.author.id)
-            except Exception:
-                pass
-            await db('UPDATE customisation SET edge=$1 WHERE user_id=$2', hex_colour, ctx.author.id)
-        tree.edge = hex_colour
-        await ctx.send("Customisation saved.") 
-        CustomisedTreeUser.all_users[ctx.author.id] = tree
+        await self.coloursetter(ctx, colour, 'edge')
 
 
     @customise.command(aliases=['user', 'person'])
-    async def node(self, ctx:Context, colour:str=None):
+    async def node(self, ctx:Context, *, colour:str=None):
         '''
         Changes the colour of users
         '''
         
-        tree = CustomisedTreeUser.get(ctx.author.id)
-        if colour == None:
-            if tree.node == None:
-                await ctx.send("You already have that set to the default colour.")
-                return 
-            tree.node = None 
-            await ctx.send("Set to the default colour.")
-            return 
-
-        # Run through checks and make sure it's a real colour
-        colour = colour.strip('#')
-        if len(colour) != 6:
-            await ctx.send("This command only accepts a valid hex code.") 
-            return
-        try:
-            hex_colour = int(colour, 16)
-        except ValueError:
-            await ctx.send("This command only accepts a valid hex code.") 
-            return
-
-        # Valid colour, save
-        async with self.bot.database() as db:
-            try:
-                await db('INSERT INTO customisation (user_id) VALUES ($1)', ctx.author.id)
-            except Exception:
-                pass
-            await db('UPDATE customisation SET node=$1 WHERE user_id=$2', hex_colour, ctx.author.id)
-        tree.node = hex_colour
-        await ctx.send("Customisation saved.") 
-        CustomisedTreeUser.all_users[ctx.author.id] = tree
+        await self.coloursetter(ctx, colour, 'node')
 
 
     @customise.command(aliases=['fontcolour', 'fontcolor'])
-    async def font(self, ctx:Context, colour:str=None):
+    async def font(self, ctx:Context, *, colour:str=None):
         '''
         Changes the colour of the font
         '''
         
-        tree = CustomisedTreeUser.get(ctx.author.id)
-        if colour == None:
-            if tree.font == None:
-                await ctx.send("You already have that set to the default colour.")
-                return 
-            tree.font = None 
-            await ctx.send("Set to the default colour.")
-            return 
-
-        # Run through checks and make sure it's a real colour
-        colour = colour.strip('#')
-        if len(colour) != 6:
-            await ctx.send("This command only accepts a valid hex code.") 
-            return
-        try:
-            hex_colour = int(colour, 16)
-        except ValueError:
-            await ctx.send("This command only accepts a valid hex code.") 
-            return
-
-        # Valid colour, save
-        async with self.bot.database() as db:
-            try:
-                await db('INSERT INTO customisation (user_id) VALUES ($1)', ctx.author.id)
-            except Exception:
-                pass
-            await db('UPDATE customisation SET font=$1 WHERE user_id=$2', hex_colour, ctx.author.id)
-        tree.font = hex_colour
-        await ctx.send("Customisation saved.") 
-        CustomisedTreeUser.all_users[ctx.author.id] = tree
+        await self.coloursetter(ctx, colour, 'font')
 
 
     @customise.command(aliases=['highlightedfont', 'highfont', 'highlightfont'])
-    async def hfont(self, ctx:Context, colour:str=None):
+    async def hfont(self, ctx:Context, *, colour:str=None):
         '''
         Chagnes the colour of the highlighted user's font
         '''
         
-        tree = CustomisedTreeUser.get(ctx.author.id)
-        if colour == None:
-            if tree.highlighted_font == None:
-                await ctx.send("You already have that set to the default colour.")
-                return 
-            tree.highlighted_font = None 
-            await ctx.send("Set to the default colour.")
-            return 
-
-        # Run through checks and make sure it's a real colour
-        colour = colour.strip('#')
-        if len(colour) != 6:
-            await ctx.send("This command only accepts a valid hex code.") 
-            return
-        try:
-            hex_colour = int(colour, 16)
-        except ValueError:
-            await ctx.send("This command only accepts a valid hex code.") 
-            return
-
-        # Valid colour, save
-        async with self.bot.database() as db:
-            try:
-                await db('INSERT INTO customisation (user_id) VALUES ($1)', ctx.author.id)
-            except Exception:
-                pass
-            await db('UPDATE customisation SET highlighted_font=$1 WHERE user_id=$2', hex_colour, ctx.author.id)
-        tree.highlighted_font = hex_colour
-        await ctx.send("Customisation saved.") 
-        CustomisedTreeUser.all_users[ctx.author.id] = tree
+        await self.coloursetter(ctx, colour, 'highlighted_font')
 
 
     @customise.command(aliases=['highlightednode', 'highnode', 'highlightnode', 'highlightedperson', 'highperson', 'highlightperson', 'highlighteduser', 'highuser', 'highlightuser'])
-    async def hnode(self, ctx:Context, colour:str=None):
+    async def hnode(self, ctx:Context, *, colour:str=None):
         '''
         Changes the colour of the highlighted user's node
         '''
         
-        tree = CustomisedTreeUser.get(ctx.author.id)
-        if colour == None:
-            if tree.highlighted_node == None:
-                await ctx.send("You already have that set to the default colour.")
-                return 
-            tree.highlighted_node = None 
-            await ctx.send("Set to the default colour.")
-            return 
-
-        # Run through checks and make sure it's a real colour
-        colour = colour.strip('#')
-        if len(colour) != 6:
-            await ctx.send("This command only accepts a valid hex code.") 
-            return
-        try:
-            hex_colour = int(colour, 16)
-        except ValueError:
-            await ctx.send("This command only accepts a valid hex code.") 
-            return
-
-        # Valid colour, save
-        async with self.bot.database() as db:
-            try:
-                await db('INSERT INTO customisation (user_id) VALUES ($1)', ctx.author.id)
-            except Exception:
-                pass
-            await db('UPDATE customisation SET highlighted_node=$1 WHERE user_id=$2', hex_colour, ctx.author.id)
-        tree.highlighted_node = hex_colour
-        await ctx.send("Customisation saved.") 
-        CustomisedTreeUser.all_users[ctx.author.id] = tree
+        await self.coloursetter(ctx, colour, 'highlighted_node')
 
 
     @customise.command(aliases=['background'])
-    async def bg(self, ctx:Context, colour:str=None):
+    async def bg(self, ctx:Context, *, colour:str=None):
         '''
         Changes the colour of the background of the tree
         '''
-        tree = CustomisedTreeUser.get(ctx.author.id)
-        if colour == None:
-            if tree.background == None:
-                await ctx.send("You already have that set to the default colour.")
-                return 
-            tree.background = None 
-            await ctx.send("Set to the default colour.")
-            return 
 
-        # Run through checks and make sure it's a real colour
-        colour = colour.strip('#')
-        if len(colour) != 6:
-            await ctx.send("This command only accepts a valid hex code.") 
-            return
-        try:
-            hex_colour = int(colour, 16)
-        except ValueError:
-            await ctx.send("This command only accepts a valid hex code.") 
-            return
-
-        # Valid colour, save
-        async with self.bot.database() as db:
-            try:
-                await db('INSERT INTO customisation (user_id) VALUES ($1)', ctx.author.id)
-            except Exception:
-                pass
-            await db('UPDATE customisation SET background=$1 WHERE user_id=$2', hex_colour, ctx.author.id)
-        tree.background = hex_colour
-        await ctx.send("Customisation saved.") 
-        CustomisedTreeUser.all_users[ctx.author.id] = tree
+        await self.coloursetter(ctx, colour, 'background')
 
 
 def setup(bot:CustomBot):
