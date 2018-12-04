e374b124ac2edbde88e7eff0954efa8f5bf7d438
iHeroGH <32426140+iHeroGH@users.noreply.github.com>
2018-12-03 21:16:40 -0800

    Update simulation.py

--- cogs/simulation.py
+++ cogs/simulation.py
@@ -21,6 +21,44 @@ class Simulation(object):
 
         await ctx.send(f"*Feeds {member} some candy")
 
+    @command()
+    @cooldown(1, 5, BucketType.user)
+    async def hug(self, member:Member):
+        '''
+        Hugs a mentioned user
+        '''
+
+        await ctx.send(f"*Hugs {member}")
+
+
+    @command()
+    @cooldown(1, 5, BucketType.user)
+    async def slap(self, member:Member):
+        '''
+        Slaps a mentioned user
+        '''
+
+        await ctx.send(f"*Slaps {member}")
+
+    @command()
+    @cooldown(1, 5, BucketType.user)
+    async def slap(self, member:Member):
+        '''
+        Slaps a mentioned user
+        '''
+
+        await ctx.send(f"*Slaps {member}")
+
+    @command()
+    @cooldown(1, 5, BucketType.user)
+    async def punch(self, member:Member):
+        '''
+        Punches a mentioned user
+        '''
+
+        await ctx.send(f"*Punches {member} right in the nose")
+
+
 
 def setup(bot:CustomBot):
     x = Simulation(bot)
