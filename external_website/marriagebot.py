0a13f1731c0dd0f7d2c5bb19056ce024428949fd
Caleb Bartlett <coolum444@hotmail.co.uk>
2018-12-04 02:28:02 +0000

    Linting changes to website code

--- external_website/marriagebot.py
+++ external_website/marriagebot.py
@@ -1,23 +1,26 @@
 import requests
+
+
 class Marriagebot(object):
+    
     base_url = "http://vps.callumb.co.uk:5696"
 
     '''
     gets a json object of all the colours from the marriagebot api
     '''
-    @staticmethod
-    def get_colours(user_id):
+    @classmethod
+    def get_colours(cls, user_id):
         params = {
             "user_id":user_id
         }
-        response = requests.get(url = (Marriagebot.base_url+"/user"),params = params)
+        response = requests.get(url = (cls.base_url+"/user"),params = params)
         return response.json()
 
     '''
     sends the user selected colours back to the marriagebot api
     '''
-    @staticmethod
-    def set_colours(user_id,
+    @classmethod
+    def set_colours(cls, user_id,
                     edge,
                     node,
                     font,
@@ -35,29 +38,29 @@ class Marriagebot(object):
             "highlighted_font":highlighted_font
         }
 
-        requests.post(url = Marriagebot.base_url+"/user",json = payload)
+        requests.post(url = cls.base_url+"/user",json = payload)
         return
     
     '''
     sends the user selected prefix to the marriagebot api
     '''
-    @staticmethod
-    def set_prefix(prefix,user_id,guild_id):
+    @classmethod
+    def set_prefix(cls, prefix,user_id,guild_id):
         payload = {
             "user_id":user_id,
             "guild_id":guild_id,
             "prefix":prefix
         }
 
-        requests.post(url = Marriagebot.base_url+"/guild",json = payload)
+        requests.post(url = cls.base_url+"/guild",json = payload)
         return
 
-    @staticmethod
-    def get_prefix(guild_id):
+    @classmethod
+    def get_prefix(cls, guild_id):
         params = {
             "guild_id":guild_id
         }
-        response = requests.get(url = Marriagebot.base_url+"/guild",params=params)
+        response = requests.get(url = cls.base_url+"/guild",params=params)
         json = response.json()
         print(response.text)
         return json["guild_prefix"]
