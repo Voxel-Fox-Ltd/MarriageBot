ad267b95507655c5ee8649114feae82cedfbb427
iHeroGH <32426140+iHeroGH@users.noreply.github.com>
2018-12-06 17:22:21 -0800

    Update information.py

--- cogs/information.py
+++ cogs/information.py
@@ -160,9 +160,11 @@ class Information(object):
         '''
         Gets the relationship between the two specified users
         '''
+
         if user == ctx.author:
             await ctx.send(f"You are you...")
             return
+
         if other == None:
             user, other = ctx.author, user
         user, other = FamilyTreeMember.get(user.id), FamilyTreeMember.get(other.id)
