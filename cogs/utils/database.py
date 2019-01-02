5e6c0d80e65a393fdaa1904161e75ae085407b9e
Callum Bartlett <coolum444@hotmail.co.uk>
2019-01-02 01:35:09 +0000

    Create pool class in database and cleaned up classes

--- cogs/utils/database.py
+++ cogs/utils/database.py
@@ -1,40 +1,70 @@
-from random import choice
+from random import choices
 from asyncio import create_subprocess_exec, get_event_loop
 
 from discord import Member
-from asyncpg import connect
+from asyncpg import connect as _connect, Connection, create_pool as _create_pool
+from asyncpg.pool import Pool
 
 
 RANDOM_CHARACTERS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-_'
 
 
+class DatabasePoolHolder(object):
+
+    def __init__(self, pool:Pool):
+        self.pool = pool
+
+
+    async def acquire(self):
+        conn = await self.pool.acquire()
+        return DatabaseConnection(conn, self)
+
+
+    async def __aenter__(self):
+        return self 
+
+    
+    async def __aexit__(self, exc_type, exc, tb):
+        await self.pool.close()
+
+
 class DatabaseConnection(object):
 
     config = None
-    restarting = False
 
-    def __init__(self):
-        self.db = None
+
+    def __init__(self, connection:Connection=None, parent:DatabasePoolHolder=None):
+        self.conn = connection
+        self.parent = parent
+
+
+    @staticmethod
+    async def create_pool(self):
+        pool = await _create_pool(**self.config)
+        return DatabasePoolHolder(pool)
+
+
+    async def connect(self):
+        self.conn = await _connect(**self.config)
+
+
+    async def close(self):
+        await self.conn.close()
+
 
     async def __aenter__(self):
-        try:
-            self.db = await connect(**self.config)
-        except Exception as e:
-            # Database connection failed - restart Postgres connection
-            if self.restarting:
-                raise Exception("Database in the process of restarting, please try again in a moment.")
-            self.restarting = True
-            restart_loop = await create_subprocess_exec(*[
-                "systemctl", "restart", "postgresql"
-            ], loop=get_event_loop())
-            await restart_loop.wait()
-            self.restarting = False
-            raise e
+        if not self.conn:
+            self.conn = await _connect(**self.config)
         return self
 
+
     async def __aexit__(self, exc_type, exc, tb):
-        await self.db.close()
-        self.db = None
+        if self.parent:
+            await self.parent.release(self.conn)
+        else:
+            await self.conn.close()
+        self.conn = None
+
 
     async def __call__(self, sql:str, *args):
         '''
@@ -42,14 +72,15 @@ class DatabaseConnection(object):
         '''
 
         # Runs the SQL
-        x = await self.db.fetch(sql, *args)
+        x = await self.conn.fetch(sql, *args)
 
         # If it got something, return the dict, else None
         if x:
             return x
-        if 'select' in sql.lower().split(' ') or 'returning' in sql.lower().split(' '):
+        if 'select' in sql.casefold() or 'returning' in sql.casefold():
             return []
         return None
+
 
     async def destroy(self, user_id:int):
         '''
@@ -59,18 +90,19 @@ class DatabaseConnection(object):
         await self('UPDATE marriages SET valid=False WHERE user_id=$1 OR partner_id=$1', user_id)
         await self('DELETE FROM parents WHERE child_id=$1 OR parent_id=$1', user_id)
 
+
     async def make_id(self, table:str, id_field:str) -> str:
         '''
         Makes a random ID that hasn't appeared in the database before for a given table
         '''
 
         while True:
-            idnumber = ''
-            for i in range(11):
-                idnumber += choice(RANDOM_CHARACTERS)
-            x = await self(f'SELECT * FROM {table} WHERE {id_field}=$1', idnumber)
+            id_number = ''.join(choices(RANDOM_CHARACTERS, k=11))
+            x = await self(f'SELECT * FROM {table} WHERE {id_field}=$1', id_number)
             if not x:
-                return idnumber
+                break
+        return id_number
+
 
     async def marry(self, instigator:Member, target:Member, marriage_id:str=None):
         '''
@@ -79,15 +111,15 @@ class DatabaseConnection(object):
         '''
 
         if marriage_id == None:
-            idnumber = await self.make_id('marriages', 'marriage_id')
+            id_number = await self.make_id('marriages', 'marriage_id')
         else:
-            idnumber = marriage_id
+            id_number = marriage_id
         # marriage_id, user_id, user_name, partner_id, partner_name, valid
         await self(
             'INSERT INTO marriages VALUES ($1, $2, $3, TRUE)',
-            idnumber,
+            id_number,
             instigator.id,
             target.id,
         )
         if marriage_id == None:
-            await self.marry(target, instigator, idnumber)  # Run it again with instigator/target flipped
+            await self.marry(target, instigator, id_number)  # Run it again with instigator/target flipped
