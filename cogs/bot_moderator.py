834e1a776b9d42dafa674e06b01991866f9ff470
Callum Bartlett <coolum444@hotmail.co.uk>
2019-01-03 19:17:00 +0000

    Run checks before forcemarry commands Closes #20

--- cogs/bot_moderator.py
+++ cogs/bot_moderator.py
@@ -41,14 +41,23 @@ class ModeratorOnly(object):
         Marries the two specified users
         '''
 
+        # Get users
+        me = FamilyTreeMember.get(user_a.id)
+        them = FamilyTreeMember.get(user_b.id)
+
+        # See if they have partners
+        if me.partner != None or them.partner != None:
+            await ctx.send("One of those users already has a partner.")
+            return
+
+        # Update database
         async with self.bot.database() as db:
             try:
                 await db.marry(user_a, user_b)
             except Exception as e:
+                await ctx.send(f"Error encountered: `{e}`")
                 return  # Only thrown if two people try to marry at once, so just return
-        me = FamilyTreeMember.get(user_a.id)
         me._partner = user_b.id 
-        them = FamilyTreeMember.get(user_b.id)
         them._partner = user_a.id
         await ctx.send("Consider it done.")
 
@@ -59,15 +68,19 @@ class ModeratorOnly(object):
         Divorces a user from their spouse
         '''
 
-        async with self.bot.database() as db:
-            try:
-                await db('DELETE FROM marriages WHERE user_id=$1 OR partner_id=$1', user.id)
-            except Exception as e:
-                return  # Honestly this should never be thrown unless the database can't connect
+        # Run check
         me = FamilyTreeMember.get(user.id)
         if not me.partner:
             await ctx.send("That person isn't even married .-.")
             return
+
+        # Update database
+        async with self.bot.database() as db:
+            try:
+                await db('DELETE FROM marriages WHERE user_id=$1 OR partner_id=$1', user.id)
+            except Exception as e:
+                await ctx.send(f"Error encountered: `{e}`")
+                return  # Honestly this should never be thrown unless the database can't connect
         me.partner._partner = None 
         me._partner = None
         await ctx.send("Consider it done.")
@@ -79,6 +92,13 @@ class ModeratorOnly(object):
         Adds the child to the specified parent
         '''
 
+        # Run check
+        them = FamilyTreeMember.get(child.id)
+        if them.parent:
+            await ctx.send("`{child!s}` already has a parent.")
+            return
+
+        # Update database
         async with self.bot.database() as db:
             try:
                 await db('INSERT INTO parents (parent_id, child_id) VALUES ($1, $2)', parent.id, child.id)
@@ -86,7 +106,6 @@ class ModeratorOnly(object):
                 return  # Only thrown when multiple people do at once, just return
         me = FamilyTreeMember.get(parent.id)
         me._children.append(child.id)
-        them = FamilyTreeMember.get(child.id)
         them._parent = parent.id
         await ctx.send("Consider it done.")
 
@@ -97,15 +116,18 @@ class ModeratorOnly(object):
         Force emancipates a child
         '''
 
+        # Run check
+        me = FamilyTreeMember.get(user.id)
+        if not me.parent:
+            await ctx.send("That user doesn't even have a parent .-.")
+            return
+
+        # Update database
         async with self.bot.database() as db:
             try:
                 await db('DELETE FROM parents WHERE child_id=$1', user.id)
             except Exception as e:
-                return  # Should only be thrown when the database can't connect
-        me = FamilyTreeMember.get(user.id)
-        if not me.parent:
-            await ctx.send("That user doesn't even have a parent .-.")
-            return 
+                return  # Should only be thrown when the database can't connect 
         me.parent._children.remove(user.id)
         me._parent = None
         await ctx.send("Consider it done.")
