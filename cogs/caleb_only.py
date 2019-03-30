9ea9116f021d28322971b2bdac0bc314c47e97cc
Caleb Bartlett <coolum444@hotmail.co.uk>
2019-03-30 21:23:18 +0000

    Fancy up the ev command

--- cogs/caleb_only.py
+++ cogs/caleb_only.py
@@ -1,5 +1,8 @@
 from traceback import format_exc
 from asyncio import iscoroutine, wait_for
+from io import StringIO 
+from textwrap import indent
+from contextlib import redirect_stdout
 
 from aiohttp import ClientSession
 from discord import Member, Message, Activity, ActivityType, User, Status, Embed
@@ -19,7 +22,7 @@ class CalebOnly(Cog):
 
     def __init__(self, bot:CustomBot):
         self.bot = bot
-        self.last_tree = None
+        self._last_result = None
 
 
     async def cog_command_error(self, ctx:Context, error):
@@ -67,36 +70,82 @@ class CalebOnly(Cog):
         await ctx.send("Printed to console.")
 
 
+    def cleanup_code(self, content):
+        """Automatically removes code blocks from the code."""
+
+        # remove ```py\n```
+        if content.startswith('```') and content.endswith('```'):
+            if content[-4] == '\n':
+                return '\n'.join(content.split('\n')[1:-1])
+            return '\n'.join(content.split('\n')[1:]).rstrip('`')
+
+        # remove `foo`
+        return content.strip('` \n')
+
+
     @command()
     async def ev(self, ctx:Context, *, content:str):
         '''
-        Runs some text through Python's eval function
+        Evaluates some Python code
+
+        Gracefully stolen from Rapptz -> 
+        https://github.com/Rapptz/RoboDanny/blob/rewrite/cogs/admin.py#L72-L117
         '''
 
+        # Make the environment
+        env = {
+            'bot': self.bot,
+            'ctx': ctx,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'guild': ctx.guild,
+            'message': ctx.message,
+            '_': self._last_result,
+            'self': self,
+        }
+        env.update(globals())
+
+        # Make code and output string
+        content = self.cleanup_code(content)
+        stdout = StringIO()
+        to_compile = f'async def func():\n{indent(content, "  ")}'
+
+        # Make the function into existence
         try:
-            ans = eval(content, globals(), locals())
+            exec(to_compile, env)
         except Exception as e:
-            await ctx.send('```py\n' + format_exc() + '```')
-            return
-        if iscoroutine(ans):
-            ans = await ans
-        await ctx.send('```py\n' + str(ans) + '```')
+            return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
 
-
-    @command()
-    async def nev(self, ctx:Context, *, content:str):
-        '''
-        Runs some text through Python's eval function
-        '''
-
+        # Grab the function we just made and run it
+        func = env['func']
         try:
-            ans = eval(content, globals(), locals())
+            # Shove stdout into StringIO
+            with redirect_stdout(stdout):
+                ret = await func()
         except Exception as e:
-            await ctx.send('```py\n' + format_exc() + '```')
-            return
-        if iscoroutine(ans):
-            ans = await ans
-        await ctx.send(str(ans))
+            # Oh no it caused an error
+            value = stdout.getvalue()
+            await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+        else:
+            # Oh no it didn't cause an error
+            value = stdout.getvalue()
+
+            # Give reaction just to show that it ran
+            try:
+                await ctx.message.add_reaction('\u2705')
+            except:
+                pass
+
+            # If the function returned nothing
+            if ret is None:
+                # It might have printed something
+                if value:
+                    await ctx.send(f'```py\n{value}\n```')
+
+            # If the function did return a value
+            else:
+                self._last_result = ret
+                await ctx.send(f'```py\n{value}{ret}\n```')
 
 
     @command(aliases=['rld'])
