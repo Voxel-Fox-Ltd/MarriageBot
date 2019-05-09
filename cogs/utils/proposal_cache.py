ff21537e3ebbfce15f1a65bd60eec64c23369d46
Caleb Bartlett <coolum444@hotmail.co.uk>
2019-05-10 00:43:50 +0100

    Add internal timeout to proposals

--- cogs/utils/proposal_cache.py
+++ cogs/utils/proposal_cache.py
@@ -1,4 +1,5 @@
 from typing import Union
+from datetime import datetime as dt, timedelta
 
 from discord import User
 
@@ -12,28 +13,47 @@ def get_id(user:Union[User, int]):
 
 class ProposalCache(dict):
 
+
     bot = None
+
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+
+    def get(self, key, d=None,*args, **kwargs):
+        item = super().get(key, d, *args, **kwargs)
+        if item in [None, d]:
+            return item
+        if len(item) == 2:
+            return item 
+        if dt.now() > item[2]:
+            return d
+        return item
+
+
     async def add(self, instigator:Union[User, int], target:Union[User, int], cog:str):
+        timeout_time = dt.now() + timedelta(seconds=60)
         async with self.bot.redis() as re:
             await re.publish_json('ProposalCacheAdd', {
                 'instigator': get_id(instigator),
                 'target': get_id(target),
-                'cog': cog
+                'cog': cog,
+                'timeout_time': timeout_time.isoformat()
             })
-        self.raw_add(instigator, target, cog)
+        self.raw_add(instigator, target, cog, timeout_time)
 
 
-    def raw_add(self, instigator:Union[User, int], target:Union[User, int], cog:str):
+    def raw_add(self, instigator:Union[User, int], target:Union[User, int], cog:str, timeout_time:dt):
         '''Adds a user to the proposal cache'''
 
         # Add to cache
-        self[get_id(instigator)] = ('INSTIGATOR', cog)
-        self[get_id(target)] = ('TARGET', cog)
+        if isinstance(timeout_time, str):
+            timeout_time = dt.strptime(timeout_time, "%Y-%m-%dT%H:%M:%S.%f")
+        self[get_id(instigator)] = ('INSTIGATOR', cog, timeout_time)
+        self[get_id(target)] = ('TARGET', cog, timeout_time)
     
+
     async def remove(self, *elements):
         async with self.bot.redis() as re:
             await re.publish_json('ProposalCacheRemove', [get_id(i) for i in elements])
