174e4d3ba75bb27e7f1d5ce612b51cdbcb70dc05
Caleb Bartlett <coolum444@hotmail.co.uk>
2019-05-16 01:31:14 +0100

    Add clean_prefix property to custom context

--- cogs/utils/custom_context.py
+++ cogs/utils/custom_context.py
@@ -12,7 +12,30 @@ class NoOutputContext(Context):
 
 class CustomContext(Context):
 
-    async def send(self, content=None, *, tts=False, embed=None, file=None, files=None, delete_after=None, nonce=None, embeddify:bool=True, embed_image:bool=True, ignore_error:bool=False):
+
+    def _set_footer(self, embed:Embed):
+        '''Sets the custom footer for the embed based on the bot config'''
+
+        # Set footer
+        try:
+            possible_footer_objects = [[i] * i.get('amount', 1) for i in self.bot.config['footer']]  # Get from config
+            footer_text_amount = []  # Make list for text 
+            [footer_text_amount.extend(i) for i in possible_footer_objects]  # Flatten list
+            footer_text = [{'text': i['text']} for i in footer_text_amount]  # Remove 'amount'
+
+            # Make sure it's not empty
+            if len(footer_text) == 0:
+                raise Exception('Make default text')
+        except Exception:
+            footer_text = [{'text': 'MarriageBot'}]
+        footer = choice(footer_text)
+        footer.update({'icon_url': self.bot.user.avatar_url})
+        footer['text'] = footer['text'].replace('{prefix}', self.prefix).replace(f"<@!{self.bot.user.id}> ", f"<@{self.bot.user.id}> ").replace(f"<@{self.bot.user.id}> ", f"@{self.bot.user!s} ")
+        embed.set_footer(**footer)
+        return embed
+
+
+    async def send(self, content=None, *, tts=False, embed=None, file=None, files=None, delete_after=None, nonce=None, embeddify:bool=True, embed_image:bool=True, ignore_error:bool=False, image_url:str=None):
         '''
         A custom version of Context that changes .send to embed things for me
         '''
@@ -41,23 +64,9 @@ class CustomContext(Context):
         if embed is None and embeddify:
             # Set content
             embed = Embed(description=content, colour=randint(1, 0xffffff))
-
-            # Set footer
-            try:
-                possible_footer_objects = [[i] * i.get('amount', 1) for i in self.bot.config['footer']]  # Get from config
-                footer_text_amount = []  # Make list for text 
-                [footer_text_amount.extend(i) for i in possible_footer_objects]  # Flatten list
-                footer_text = [{'text': i['text']} for i in footer_text_amount]  # Remove 'amount'
-
-                # Make sure it's not empty
-                if len(footer_text) == 0:
-                    raise Exception('Make default text')
-            except Exception:
-                footer_text = [{'text': 'MarriageBot'}]
-            footer = choice(footer_text)
-            footer.update({'icon_url': self.bot.user.avatar_url})
-            footer['text'] = footer['text'].replace('{prefix}', self.prefix).replace(f"<@!{self.bot.user.id}> ", f"<@{self.bot.user.id}> ").replace(f"<@{self.bot.user.id}> ", f"@{self.bot.user!s} ")
-            embed.set_footer(**footer)
+            embed = self._set_footer(embed)
+            if image_url:
+                embed.set_image(url=image_url)
 
             # Set image
             if file and embed_image:
@@ -66,6 +75,12 @@ class CustomContext(Context):
 
             # Reset content
             content = self.bot.config.get("embed_default_text") or None
+
+        elif embed and embeddify:
+            if embed.footer.text:
+                pass 
+            else:
+                embed = self._set_footer(embed)
 
         try:
             return await super().send(
@@ -79,3 +94,13 @@ class CustomContext(Context):
             )
         except Exception as e:
             if not ignore_error: raise e
+
+    @property
+    def clean_prefix(self):
+        '''The prefix used but cleaned up for if it's a mention'''
+
+        v = self.prefix 
+        if '<@' in v:
+            uid = int(v.split('>')[0].split('<@')[0])
+            return f'@{self.bot.get_user(uid)[:5]}'
+        return v
