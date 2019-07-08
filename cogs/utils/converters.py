ca9782ddc69397e2a5e257bff4be4dd80922737f
Caleb Bartlett <coolum444@hotmail.co.uk>
2019-07-09 00:38:41 +0100

    Fix up UserID converter

--- cogs/utils/converters.py
+++ cogs/utils/converters.py
@@ -1,14 +1,28 @@
-from discord.ext.commands import UserConverter, BadArgument
+from discord.ext.commands import UserConverter, BadArgument, Converter
 
 
-class UserID(int):
+class UserID(Converter):
     async def convert(self, ctx, value) -> int:
-        v = None
+
+        # Try userconverter
         try: 
-            return (await UserConverter().convert(ctx, value)).id
-        except Exception as e: 
-            try:
-                return int(value)
-            except Exception:
-                raise e
+            v = await UserConverter().convert(ctx, value) 
+            if v:
+                return v.id
+        except Exception:
+            pass
+
+        # Try int (see if it's an ID already)
+        try:
+            return int(value)
+        except ValueError:
+            pass 
+        
+        # See if it's a mention
+        try:
+            return int(value.strip('<>!@'))
+        except ValueError:
+            pass 
+
+        # Ah well
         raise BadArgument()
