fd96d19e40cc66f71daf47be55405fda1ee842a6
Callum Bartlett <coolum444@hotmail.co.uk>
2019-08-08 06:21:51 +0300

    Clean up the user ID converter

--- cogs/utils/converters.py
+++ cogs/utils/converters.py
@@ -1,28 +1,37 @@
-from discord.ext.commands import UserConverter, BadArgument, Converter
+import discord
+from discord.ext import commands
 
 
-class UserID(Converter):
-    async def convert(self, ctx, value) -> int:
+class UserID(commands.Converter):
+    """A conveter that takes the given value and tries to grab the ID from it
+
+    First it runs UserConverter, then it tries to int value, then it checks
+    if the value is a mention, before finally giving up
+
+    Returns the ID of the user"""
+
+    async def convert(self, ctx:commands.Context, value:str) -> int:
+        """Converts the given value to a valid user ID"""
 
         # Try userconverter
-        try: 
-            v = await UserConverter().convert(ctx, value) 
+        try:
+            v: discord.User = await commands.UserConverter().convert(ctx, value)
             if v:
                 return v.id
-        except Exception:
+        except commands.BadArgument:
             pass
 
         # Try int (see if it's an ID already)
         try:
             return int(value)
         except ValueError:
-            pass 
-        
+            pass
+
         # See if it's a mention
         try:
             return int(value.strip('<>!@'))
         except ValueError:
-            pass 
+            pass
 
         # Ah well
-        raise BadArgument()
+        raise commands.BadArgument()
