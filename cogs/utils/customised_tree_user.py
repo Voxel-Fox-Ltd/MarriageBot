1bf08f9ed54bdf0a6274610327cd7cc3a35c70da
Callum Bartlett <coolum444@hotmail.co.uk>
2019-08-08 06:09:31 +0300

    Clean up the customised tree user class Now with docstrings, slots, and proper defaults

--- cogs/utils/customised_tree_user.py
+++ cogs/utils/customised_tree_user.py
@@ -1,12 +1,25 @@
+import typing
+
+import asyncpg
+
+from cogs.utils.custom_bot import CustomBot
 from cogs.utils.database import DatabaseConnection as DBC
 
 
 class CustomisedTreeUser(object):
+    """A class to hold the custom tree setup for a given user"""
 
-    all_users = {}  # discord_id: CTU
-    bot = None
+    all_users: typing.Dict[int, 'CustomisedTreeUser'] = {}
+    bot: CustomBot = None
+    __slots__ = (
+        'id', 'edge', 'node', 'font', 'highlighted_font',
+        'highlighted_node', 'background', 'direction'
+    )
 
-    def __init__(self, user_id:int, *, edge:int=None, node:int=None, font:int=None, highlighted_font:int=None, highlighted_node:int=None, background:int=None, direction:str="TB"):
+    def __init__(self, user_id:int, *, edge:int=None, node:int=None,
+                 font:int=None, highlighted_font:int=None,
+                 highlighted_node:int=None, background:int=None,
+                 direction:str="TB"):
         self.id = user_id
         self.edge = edge
         self.node = node
@@ -17,9 +30,10 @@ class CustomisedTreeUser(object):
         self.direction = direction
         self.all_users[user_id] = self
 
-    
     @classmethod
     async def get(cls, key, db:DBC=None):
+        """Grabs a user's data from the database"""
+
         if db:
             data = await db('SELECT * FROM customisation WHERE user_id=$1', key)
         else:
@@ -29,38 +43,41 @@ class CustomisedTreeUser(object):
             return cls(**data[0])
         return cls(key)
 
-
-    @property 
+    @property
     def hex(self) -> dict:
-        if self.edge != None:
+        """The conversion of the user's data into some quotes hex strings
+        that can be passed directly to Graphviz
+        Provides deafults"""
+
+        if self.edge is not None:
             edge =  f'"#{self.edge:06X}"' if self.edge >= 0 else 'transparent'
         else:
-            edge = '"#000000"'
+            edge = self.get_default_hex['edge']
 
-        if self.font != None:
+        if self.font is not None:
             font =  f'"#{self.font:06X}"' if self.font >= 0 else 'transparent'
         else:
-            font = '"#000000"'
+            font = self.get_default_hex['font']
 
-        if self.node != None:
+        if self.node is not None:
             node =  f'"#{self.node:06X}"' if self.node >= 0 else 'transparent'
         else:
-            node = '"#FFFFFF"'
+            node = self.get_default_hex['node']
 
-        if self.highlighted_font != None:
+        if self.highlighted_font is not None:
             highlighted_font =  f'"#{self.highlighted_font:06X}"' if self.highlighted_font >= 0 else 'transparent'
         else:
-            highlighted_font = '"#FFFFFF"'
+            highlighted_font = self.get_default_hex['highlighted_font']
 
-        if self.highlighted_node != None:
+        if self.highlighted_node is not None:
             highlighted_node =  f'"#{self.highlighted_node:06X}"' if self.highlighted_node >= 0 else 'transparent'
         else:
-            highlighted_node = '"#0000FF"'
+            highlighted_node = self.get_default_hex['highlighted_node']
 
-        if self.background != None:
+        if self.background is not None:
             background =  f'"#{self.background:06X}"' if self.background >= 0 else 'transparent'
         else:
-            background = '"#FFFFFF"'
+            background = self.get_default_hex['background']
 
         return {
             'edge': edge,
@@ -72,14 +89,16 @@ class CustomisedTreeUser(object):
             'direction': f'"{self.direction}"'
         }
 
-    
-    @property 
+    @property
     def unquoted_hex(self):
+        """The same as self.hex, but strips out the quote marks from the items"""
+
         return {i: o.strip('"') for i, o in self.hex.items()}
 
-    
-    @classmethod 
+    @classmethod
     def get_default_hex(self):
+        """The default hex codes that are used, quoted"""
+
         return {
             'edge': '"#000000"',
             'node': '"#000000"',
@@ -90,14 +109,15 @@ class CustomisedTreeUser(object):
             'direction': '"TB"',
         }
 
-
-    @classmethod 
+    @classmethod
     def get_default_unquoted_hex(cls):
+        """The default hex codes that are used, unquoted"""
+
         return {i: o.strip('"') for i, o in cls.get_default_hex().items()}
 
 
     async def save(self, db:DBC):
-        '''Saves this data to database'''
+        """Saves all this lovely cached data into the database"""
 
         try:
             await db(
@@ -105,7 +125,7 @@ class CustomisedTreeUser(object):
                 VALUES ($1, $2, $3, $4, $5, $6, $7, $8)''',
                 self.id, self.edge, self.node, self.font, self.highlighted_font, self.highlighted_node, self.background, self.direction
             )
-        except Exception:
+        except asyncpg.UniqueViolationError:
             await db(
                 '''UPDATE customisation SET edge=$2, node=$3, font=$4, highlighted_font=$5, highlighted_node=$6, background=$7, direction=$8 WHERE user_id=$1''',
                 self.id, self.edge, self.node, self.font, self.highlighted_font, self.highlighted_node, self.background, self.direction
