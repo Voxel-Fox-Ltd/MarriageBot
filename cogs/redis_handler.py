6bb37b66e9821f8389e2295d786bb2158851b31f
Callum Bartlett <coolum444@hotmail.co.uk>
2019-08-08 05:46:13 +0300

    Remove tree cache util

--- cogs/redis_handler.py
+++ cogs/redis_handler.py
@@ -10,13 +10,11 @@ from cogs.utils.family_tree.family_tree_member import FamilyTreeMember
 class RedisHandler(Cog):
 
     def __init__(self, bot:CustomBot):
-        self.bot = bot 
+        self.bot = bot
         super().__init__(__class__.__name__)
         task = bot.loop.create_task
         self.channels = []  # Populated automatically
         self.handlers = [
-            # task(self.channel_handler('TreeCacheAdd', lambda data: bot.tree_cache.raw_add(*data))),
-            # task(self.channel_handler('TreeCacheRemove', lambda data: bot.tree_cache.raw_remove(*data))),
             task(self.channel_handler('RunGlobalCommand', self.run_global_command)),
             task(self.channel_handler('DBLVote', lambda data: bot.dbl_votes.__setitem__(data['user_id'], dt.strptime(data['datetime'], "%Y-%m-%dT%H:%M:%S.%f")))),
             task(self.channel_handler('TriggerStartup', self.trigger_startup)),
@@ -39,7 +37,7 @@ class RedisHandler(Cog):
 
     async def channel_handler(self, channel_name:str, function:callable, log:bool=True, *args, **kwargs):
         '''
-        General handler for creating a channel, waiting for an input, and then 
+        General handler for creating a channel, waiting for an input, and then
         plugging the data into a function
         '''
 
@@ -57,14 +55,14 @@ class RedisHandler(Cog):
             # Get and log the data
             data = await channel.get_json()
             if log: self.log_handler.debug(f"Redis message on {channel_name}: {data!s}")
-            
+
             # Run the callable
             if iscoroutine(function) or iscoroutinefunction(function):
                 await function(data, *args, **kwargs)
             else:
                 function(data, *args, **kwargs)
 
-        
+
     async def run_global_command(self, data:dict):
         '''Runs a given command globally, across all shards'''
 
@@ -97,9 +95,9 @@ class RedisHandler(Cog):
         '''Triggers startup for a given shard id'''
 
         if data['shard_id'] and data['shard_id'] in self.shard_ids:
-            pass 
+            pass
         else:
-            return 
+            return
         await self.bot.startup()
 
 
