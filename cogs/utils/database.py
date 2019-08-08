285de8390640d96b6e9b0dd2cbce9811025fee55
Callum Bartlett <coolum444@hotmail.co.uk>
2019-08-08 05:38:04 +0300

    Clean up the database util

--- cogs/utils/database.py
+++ cogs/utils/database.py
@@ -1,61 +1,54 @@
-from random import choices
-from asyncio import create_subprocess_exec, get_event_loop
-from typing import Union, List
+import typing
 import logging
 
-from discord import Member
-from asyncpg import connect as _connect, Connection, create_pool as _create_pool
-from asyncpg.pool import Pool
+import discord
+import asyncpg
 
 
 class DatabaseConnection(object):
+    """A helper class to wrap around an asyncpg.Connection object so as
+    to make it a little easier to use"""
 
-    config = None
-    pool = None
+    config: dict = None
+    pool: asyncpg.pool.Pool = None
     logger: logging.Logger = None
 
-
-    def __init__(self, connection:Connection=None):
+    def __init__(self, connection:asyncpg.Connection=None):
         self.conn = connection
 
+    @staticmethod
+    async def create_pool(config:dict) -> None:
+        """Creates the database pool and plonks it in DatabaseConnection.pool"""
 
-    @classmethod
-    async def create_pool(cls, config:dict) -> None:
-        '''Connects the class to the system's postgres instance'''
-
-        cls.config = config
-        cls.pool = await _create_pool(**config)
-
+        DatabaseConnection.config = config
+        DatabaseConnection.pool = await asyncpg.create_pool(**config)
 
     @classmethod
     async def get_connection(cls) -> 'DatabaseConnection':
-        '''Gets a connection from the connection pool'''
+        """Acquires a connection to the database from the pool"""
 
         conn = await cls.pool.acquire()
         return cls(conn)
 
-
     async def disconnect(self) -> None:
-        '''Releases a connection from the database pool'''
+        """Releases a connection from the pool back to the mix"""
 
         await self.pool.release(self.conn)
         self.conn = None
         del self
 
-
     async def __aenter__(self):
         self.conn = await self.pool.acquire()
         return self
-
 
     async def __aexit__(self, exc_type, exc, tb):
         await self.pool.release(self.conn)
         self.conn = None
         del self
 
-
-    async def __call__(self, sql:str, *args) -> Union[List[dict], None]:
-        '''Runs a line of SQL using the internal database'''
+    async def __call__(self, sql:str, *args) -> typing.Union[typing.List[dict], None]:
+        """Runs a line of SQL and returns a list, if things are expected back,
+        or None, if nothing of interest is happening"""
 
         # Runs the SQL
         self.logger.debug(f"Running SQL: {sql} {args!s}")
@@ -68,20 +61,14 @@ class DatabaseConnection(object):
             return []
         return None
 
-
-    async def destroy(self, user_id:int):
-        '''Removes a given user ID form all parts of the database'''
-
-        await self('DELETE FROM marriages WHERE user_id=$1 OR partner_id=$1', user_id)
-        await self('DELETE FROM parents WHERE child_id=$1 OR parent_id=$1', user_id)
-
-
-    async def marry(self, instigator:Member, target:Member, guild_id:int):
-        '''Marries two users together'''
+    async def marry(self, instigator:typing.Union[int, discord.User],
+                    target:typing.Union[int, discord.User], guild_id:int):
+        """Marries two given Discord users together"""
 
         instigator_id = getattr(instigator, 'id', instigator)
         target_id = getattr(target, 'id', target)
 
+        # TODO make this a transaction
         await self(
             'INSERT INTO marriages (user_id, partner_id, guild_id) VALUES ($1, $2, $3)',
             instigator_id, target_id, guild_id,
