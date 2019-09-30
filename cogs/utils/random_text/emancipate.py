06f77d3ed947719039ab6f61331834138a4f6461
Caleb Bartlett <coolum444@hotmail.co.uk>
2019-09-30 02:46:58 +0100

    Update emancipate random text

--- cogs/utils/random_text/emancipate.py
+++ cogs/utils/random_text/emancipate.py
@@ -1,32 +1,24 @@
-from random import choice
-
-from cogs.utils.random_text.text_template import TextTemplate
+from cogs import utils
 
 
-class EmancipateRandomText(TextTemplate):
+class EmancipateRandomText(utils.random_text.TextTemplate):
 
-
-    @classmethod
-    def valid_target(cls, instigator=None, target=None):
-        '''
-        '''
-
-        return choice(cls.get_valid_strings([
+    @staticmethod
+    @utils.random_text.get_random_valid_string
+    def valid_target(instigator=None, target=None):
+        return [
             "Looks like {instigator.mention} left you, {target.mention}. I'm sorry for your loss.",
             "You're free of {target.mention}, {instigator.mention}!",
             "Say goodbye to {target.mention}, {instigator.mention}! You're parentless now!",
             "Freedom for you, {instigator.mention}!",
             "Have fun living in the streets!",
             "You no longer have a parent.\n... Don't think too hard about it.",
-        ], *['instigator' if instigator else None, 'target' if target else None])).format(instigator=instigator, target=target)
+        ]
 
-
-    @classmethod
-    def instigator_is_unqualified(cls, instigator=None, target=None):
-        '''
-        '''
-
-        return choice(cls.get_valid_strings([
+    @staticmethod
+    @utils.random_text.get_random_valid_string
+    def instigator_is_unqualified(instigator=None, target=None):
+        return [
             "You don't actually have a parent. This is awkward.",
             "You're already an orphan though!",
-        ], *['instigator' if instigator else None, 'target' if target else None])).format(instigator=instigator, target=target)
+        ]
