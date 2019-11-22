19dfdb0b01833a69b073fb920af3febefdb92b9f
Caleb Bartlett <coolum444@hotmail.co.uk>
2019-11-22 04:07:45 +0000

    Copy database over from MarriageBot

--- cogs/utils/database.py
+++ cogs/utils/database.py
@@ -1,62 +1,82 @@
+import typing
 import logging
 
+import discord
 import asyncpg
 
 
 class DatabaseConnection(object):
-    """A wrapper around asyncpg to let me deal with connections better"""
+    """A helper class to wrap around an asyncpg.Connection object so as
+    to make it a little easier to use"""
 
     config: dict = None
     pool: asyncpg.pool.Pool = None
     logger: logging.Logger = None
+    __slots__ = ('conn',)
 
     def __init__(self, connection:asyncpg.Connection=None):
         self.conn = connection
-        if self.logger is None:
-            self.logger = logging.getLogger("database")
-            self.__class__.logger.warning(f"No logger has been passed "
-                                           "through to {self.__class__.__name__}")
+
+    @staticmethod
+    async def create_pool(config:dict) -> None:
+        """Creates the database pool and plonks it in DatabaseConnection.pool"""
+
+        DatabaseConnection.config = config
+        DatabaseConnection.pool = await asyncpg.create_pool(**config)
+
 
     @classmethod
-    async def create_pool(cls, config:dict) -> None:
-        """Creates a database pool that'll be used to grab every instance"""
-
-        cls.config = config
-        cls.pool = await asyncpg.create_pool(**config)
-
-    @classmethod 
     async def get_connection(cls) -> 'DatabaseConnection':
-        """Gets a connection from the connection pool"""
+        """Acquires a connection to the database from the pool"""
 
         conn = await cls.pool.acquire()
         return cls(conn)
 
     async def disconnect(self) -> None:
-        """Releases a connection from the database pool"""
+        """Releases a connection from the pool back to the mix"""
 
-        await self.pool.release(self.conn)
+        if isinstance(self.conn, asyncpg.Connection):
+            await self.pool.release(self.conn)
+        elif isinstance(self.conn, asyncpg.transaction.Transaction):
+            await self.conn.commit()
+        else:
+            raise Exception("This is definitely wrong")
         self.conn = None
         del self
+
+    async def get_transaction(self) -> 'DatabaseConnection':
+        """Creates a database object for a transaction"""
+
+        tr = self.__class__(self.conn.transaction())
+        await tr.conn.start()
 
     async def __aenter__(self):
         self.conn = await self.pool.acquire()
         return self
 
     async def __aexit__(self, exc_type, exc, tb):
-        await self.pool.release(self.conn)
+        if isinstance(self.conn, asyncpg.Connection):
+            await self.pool.release(self.conn)
+        else:
+            raise Exception("This is definitely wrong")
         self.conn = None
         del self
 
-    async def __call__(self, sql:str, *args):
-        """Runs a line of SQL (with arguments) with the internal .conn connection"""
+    async def __call__(self, sql:str, *args) -> typing.Union[typing.List[dict], None]:
+        """Runs a line of SQL and returns a list, if things are expected back,
+        or None, if nothing of interest is happening"""
 
         # Runs the SQL
-        self.logger.debug(f"Running SQL: {sql} ({args!s})")
-        x = await self.conn.fetch(sql, *args)
+        self.logger.debug(f"Running SQL: {sql} {args!s}")
+        if 'select' in sql.casefold() or 'returning' in sql.casefold():
+            x = await self.conn.fetch(sql, *args)
+        else:
+            await self.conn.execute(sql, *args)
+            return
 
         # If it got something, return the dict, else None
         if x:
             return x
         if 'select' in sql.casefold() or 'returning' in sql.casefold():
-            return []  # Return an empty dict if I was *expecting* something
+            return []
         return None
