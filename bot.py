eadc4a445c0bdd5bc13d81437879c61afec6da2d
Caleb Bartlett <coolum444@hotmail.co.uk>
2019-11-22 03:47:19 +0000

    Add better handling to shard counts

--- bot.py
+++ bot.py
@@ -5,8 +5,7 @@ import os
 
 import discord
 
-from cogs.utils.custom_bot import CustomBot
-from cogs.utils.database import DatabaseConnection
+from cogs import utils
 
 
 # Set up loggers
@@ -20,26 +19,34 @@ filterwarnings('ignore', category=RuntimeWarning)
 parser = ArgumentParser()
 parser.add_argument("config_file", help="The configuration for the bot.")
 parser.add_argument(
-    "--min", type=int, default=None, 
+    "--min", type=int, default=None,
     help="The minimum shard ID that this instance will run with (inclusive)"
 )
 parser.add_argument(
-    "--max", type=int, default=None, 
+    "--max", type=int, default=None,
     help="The maximum shard ID that this instance will run with (inclusive)"
 )
 parser.add_argument(
-    "--shardcount", type=int, default=None, 
+    "--shardcount", type=int, default=None,
     help="The amount of shards that the bot should be using."
 )
 parser.add_argument("--loglevel", default="INFO")
 args = parser.parse_args()
 
-# Create bot object
-shard_ids = None if args.shardcount == None else list(range(args.min, args.max+1))
-if args.shardcount == None and (args.min or args.max):
-    logger.critical("You set a min/max shard but no shard count.")
+# Make sure the sharding info provided is correctish
+if args.shardcount is None:
+    shard_ids = None
+else:
+    shard_ids = list(range(args.min, args.max+1))
+if args.shardcount is None and (args.min or args.max):
+    logger.critical("You set a min/max shard handler but no shard count")
     exit(1)
-bot = CustomBot(
+if args.shardcount is not None and not (args.min is not None and args.max is not None):
+    logger.critical("You set a shardcount but not min/max shards")
+    exit(1)
+
+# Okay cool make the bot object
+bot = utils.CustomBot(
     config_file=args.config_file,
     activity=discord.Game(name="Reconnecting..."),
     status=discord.Status.dnd,
@@ -47,6 +54,7 @@ bot = CustomBot(
     shard_count=args.shardcount,
     shard_ids=shard_ids,
     shard_id=args.min,
+    max_messages=100,  # The lowest amount that we can actually cache
     logger=logger.getChild('bot')
 )
 
@@ -63,7 +71,7 @@ async def on_ready():
     """Run when the bot connects to Discord properly
     Sets presence to default and not a lot else"""
 
-    logger.info(f"Bot connected - {bot.user} // {bot.user.id}")    
+    logger.info(f"Bot connected - {bot.user} // {bot.user.id}")
     logger.info("Setting activity to default")
     await bot.set_default_presence()
     logger.info('Bot loaded.')
@@ -73,12 +81,12 @@ if __name__ == '__main__':
     """Starts the bot, connects the database, runs the async loop forever"""
 
     # Grab the event loop
-    loop = bot.loop 
+    loop = bot.loop
 
     # Connect the database
     logger.info("Creating database pool")
     try:
-        db_connect = DatabaseConnection.create_pool(bot.config['database'])
+        db_connect = utils.DatabaseConnection.create_pool(bot.config['database'])
         loop.run_until_complete(db_connect)
     except KeyError as e:
         logger.critical("KeyError creating database pool - "
@@ -102,7 +110,7 @@ if __name__ == '__main__':
     try:
         logger.info("Running bot")
         bot.run()
-    except KeyboardInterrupt: 
+    except KeyboardInterrupt:
         pass
 
     # We're now done running the bot, time to clean up and close
