f6756eb42ade7c9ea132996d7ab13e99827c33f7
Caleb Bartlett <coolum444@hotmail.co.uk>
2019-12-04 20:46:15 +0000

    Update webhook routes

--- website/api.py
+++ website/api.py
@@ -1,10 +1,9 @@
 from datetime import datetime as dt
 
 from aiohttp.web import Response, RouteTableDef, Request
-from ujson import dumps
-
-from cogs.utils.custom_bot import CustomBot
-from cogs.utils.customised_tree_user import CustomisedTreeUser
+import json
+import discord
+import asyncpg
 
 
 routes = RouteTableDef()
@@ -12,42 +11,43 @@ routes = RouteTableDef()
 
 @routes.post('/webhook/dbl')
 async def webhook_handler(request:Request):
-    '''
-    Sends a PM to the user with the webhook attached if user in owners
-    '''
-    
-    # Set up some stuff
+    """Sends a PM to the user with the webhook attached if user in owners"""
+
+    # Get our data
     bot = request.app['bot']
-    success = Response(
-        text=dumps({"success": True}),
-        content_type="application/json"
-    )
-    failure = lambda x: Response(
-        text=dumps({"success": False, **x}),
-        content_type="application/json"
-    )
+
+    # Set up our responses
+    success = Response(text=json.dumps({"success": True}), content_type="application/json")
+    failure = lambda x: Response(text=json.dumps({"success": False, **x}), content_type="application/json")
+
+    # See if we can get it
     try:
         x = await request.json()
     except Exception:
         return failure({'reason': 'No JSON response'})
 
     # See if it's all valid
-    if 'bot' in x and 'user' in x and 'type' in x:
-        pass 
-    else:
-        return failure({'reason': 'Invalid request params'}) # No valid points
+    keys = set(['bot', 'user', 'type'])
+    if set(x.keys()) != keys:
+        return failure({'reason': 'Invalid request params'})
+
+    # Check the bot's ID
     try:
-        if int(x['bot']) not in [bot.user.id, 488227732057227265]:
-            return failure({'reason': 'Invalid bot ID'})
+        if int(x['bot']) != bot.user.id:
+            return failure({'reason': 'Invalid bot ID'})  # wrong bot
     except ValueError:
-        # Bot ID wasn't an integer
-        return failure({'reason': 'Invalid bot ID'})
+        return failure({'reason': 'Invalid bot ID'})  # not an int
+
+    # Check user's ID
     try:
-        user = await bot.fetch_user(int(x['user']))
         user_id = int(x['user'])
+        user = await bot.fetch_user(user_id)
     except ValueError:
-        # User ID wasn't an integer
-        return failure({'reason': 'Invalid user ID'})
+        return failure({'reason': 'Invalid user ID'})  # uid wasn't int
+    except (discord.Forbidden, discord.NotFound):
+        user = None
+
+    # Check type
     if x['type'] not in ['upvote', 'test']:
         return failure({'reason': 'Invalid request type'})
 
@@ -57,18 +57,16 @@ async def webhook_handler(request:Request):
             await user.send("Thank you for upvoting MarriageBot!")
         elif x['type'] == 'test':
             await user.send("Thanks for the text ping boss.")
-    except Exception:
-        # Couldn't send them a message oops
-        pass 
+    except (discord.Forbidden, discord.NotFound, AttributeError):
+        pass  # couldn't send or not found user
 
     # Database it up
-    # bot.dbl_votes[user_id] = dt.now()
     time = dt.now()
     async with bot.redis() as re:
         await re.publish_json('DBLVote', {'user_id': user.id, 'datetime': time.isoformat()})
     async with bot.database() as db:
         try:
             await db('INSERT INTO dbl_votes (user_id, timestamp) VALUES ($1, $2)', user_id, time)
-        except Exception as e:
+        except asyncpg.UniqueViolationError:
             await db('UPDATE dbl_votes SET timestamp=$2 WHERE user_id=$1', user_id, time)
     return success
