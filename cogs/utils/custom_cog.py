2d3f8cebce4dda9585b3f807257a38beee67b00d
Caleb Bartlett <coolum444@hotmail.co.uk>
2019-12-07 15:14:17 +0000

    Add quotes to username

--- cogs/utils/custom_cog.py
+++
@@ -1,9 +0,0 @@
-from logging import getLogger
-
-from discord.ext.commands import Cog as OriginalCog
-
-
-class Cog(OriginalCog):
-
-    def __init__(self, cog_name:str):
-        self.log_handler = getLogger(f"marriagebot.cogs.{cog_name}")
