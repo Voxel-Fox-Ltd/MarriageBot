3971aa337b56e3bdd86226ba233d929053d3dd43
Caleb Bartlett <coolum444@hotmail.co.uk>
2019-12-09 03:26:01 +0000

    Fix blocked user redis

--- cogs/redis_handler.py
+++ cogs/redis_handler.py
@@ -22,8 +22,8 @@ class RedisHandler(utils.Cog):
             task(self.channel_handler('DBLVote', lambda data: bot.dbl_votes.__setitem__(data['user_id'], dt.strptime(data['datetime'], "%Y-%m-%dT%H:%M:%S.%f")))),
             task(self.channel_handler('ProposalCacheAdd', lambda data: bot.proposal_cache.raw_add(**data))),
             task(self.channel_handler('ProposalCacheRemove', lambda data: bot.proposal_cache.raw_remove(*data))),
-            task(self.channel_handler('BlockedUserAdd', lambda data: bot.blocked_users[data['user_id'].append(data['blocked_user_id'])])),
-            task(self.channel_handler('BlockedUserRemove', lambda data: bot.blocked_users[data['user_id'].remove(data['blocked_user_id'])])),
+            task(self.channel_handler('BlockedUserAdd', lambda data: bot.blocked_users[data['user_id']].append(data['blocked_user_id']))),
+            task(self.channel_handler('BlockedUserRemove', lambda data: bot.blocked_users[data['user_id']].remove(data['blocked_user_id']))),
             task(self.channel_handler('EvalAll', self.eval_all)),
             task(self.channel_handler('UpdateGuildPrefix', self.update_guild_prefix)),
         ]
