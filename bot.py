4ca842e30e9de1fd4546325eebac142a2d6d819b
Caleb Bartlett <coolum444@hotmail.co.uk>
2020-01-20 03:27:15 +0000

    Grab changes made in MarriageBot

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
-from argparse import ArgumentParser
-from warnings import filterwarnings
+import argparse
+import warnings
 import logging
 import os
 import asyncio
+import sys
 
 import discord
 
@@ -14,31 +15,50 @@ logging.basicConfig(format='%(name)s:%(levelname)s: %(message)s')
 logger = logging.getLogger(os.getcwd().split(os.sep)[-1].split()[-1].lower())
 
 # Filter warnings
-filterwarnings('ignore', category=RuntimeWarning)
+warnings.filterwarnings('ignore', category=RuntimeWarning)
+
+# Use right event loop
+if sys.platform == 'win32':
+    loop = asyncio.ProactorEventLoop()
+    asyncio.set_event_loop(loop)
 
 # Parse arguments
-parser = ArgumentParser()
-parser.add_argument("config_file", help="The configuration for the bot.")
-parser.add_argument(
-    "--min", type=int, default=None,
-    help="The minimum shard ID that this instance will run with (inclusive)"
-)
-parser.add_argument(
-    "--max", type=int, default=None,
-    help="The maximum shard ID that this instance will run with (inclusive)"
-)
-parser.add_argument(
-    "--shardcount", type=int, default=None,
-    help="The amount of shards that the bot should be using."
-)
-parser.add_argument("--loglevel", default="INFO")
-args = parser.parse_args()
+def get_program_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config_file", help="The configuration for the bot")
+    parser.add_argument(
+        "--min", type=int, default=None,
+        help="The minimum shard ID that this instance will run with (inclusive)"
+    )
+    parser.add_argument(
+        "--max", type=int, default=None,
+        help="The maximum shard ID that this instance will run with (inclusive)"
+    )
+    parser.add_argument(
+        "--shardcount", type=int, default=None,
+        help="The amount of shards that the bot should be using"
+    )
+    parser.add_argument(
+        "--loglevel", default="INFO",
+        help="Logging level for the bot - probably most useful is INFO and DEBUG"
+    )
+    parser.add_argument(
+        "--loglevel-discord", default="INFO",
+        help="Logging level for discord - probably most useful is INFO and DEBUG"
+    )
+    parser.add_argument(
+        "--loglevel-database", default="INFO",
+        help="Logging level for database - probably most useful is INFO and DEBUG"
+    )
+    return parser.parse_args()
+args = get_program_arguments()
 
 # Make sure the sharding info provided is correctish
 if args.shardcount is None:
-    shard_ids = None
-else:
-    shard_ids = list(range(args.min, args.max+1))
+    args.shardcount = 1
+    args.min = 0
+    args.max = 0
+shard_ids = list(range(args.min, args.max+1))
 if args.shardcount is None and (args.min or args.max):
     logger.critical("You set a min/max shard handler but no shard count")
     exit(1)
@@ -56,15 +76,17 @@ bot = utils.CustomBot(
     shard_ids=shard_ids,
     shard_id=args.min,
     max_messages=100,  # The lowest amount that we can actually cache
-    logger=logger.getChild('bot')
+    logger=logger.getChild('bot'),
 )
 
-# Set up our loggers
+# Set up out loggers
 log_level = getattr(logging, args.loglevel.upper(), None)
 if log_level is None:
-    raise Exception("Invalid log level provided")
+    logger.critical("An invalid log level was provided")
+    exit(1)
 logger.setLevel(log_level)
-logging.getLogger("discord").setLevel(logging.INFO)
+logging.getLogger('discord').setLevel(getattr(logging, args.loglevel_discord.upper(), log_level))
+bot.database.logger.setLevel(getattr(logging, args.loglevel_database.upper(), log_level))
 
 
 @bot.event
@@ -87,7 +109,7 @@ if __name__ == '__main__':
     # Connect the database connection
     logger.info("Creating database pool")
     try:
-        db_connect_task = asyncio.ensure_future(utils.DatabaseConnection.create_pool(bot.config['database']))
+        db_connect_task = loop.create_task(utils.DatabaseConnection.create_pool(bot.config['database']))
         loop.run_until_complete(db_connect_task)
     except KeyError as e:
         raise Exception("KeyError creating database pool - is there a 'database' object in the config?")
