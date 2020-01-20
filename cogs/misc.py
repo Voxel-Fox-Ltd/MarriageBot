20e47bff84379c70f807bf10245130cc20bd63e5
Caleb Bartlett <coolum444@hotmail.co.uk>
2020-01-20 04:06:13 +0000

    Add checks onto the misc commands

--- cogs/misc.py
+++ cogs/misc.py
@@ -5,13 +5,13 @@ from cogs import utils
 
 class Misc(utils.Cog):
 
-    @commands.command(aliases=['git', 'code'], cls=utils.Command)
+    @commands.command(aliases=['git', 'code'], cls=utils.Command, requires_set_config=['github'])
     async def github(self, ctx:utils.Context):
         """Sends the GitHub Repository link"""
 
         await ctx.send(f"<{self.bot.config.get('github')}>")
 
-    @commands.command(aliases=['support', 'guild'], cls=utils.Command)
+    @commands.command(aliases=['support', 'guild'], cls=utils.Command, requires_set_config=['guild_invite'])
     async def server(self, ctx:utils.Context):
         """Gives the invite to the support server"""
 
