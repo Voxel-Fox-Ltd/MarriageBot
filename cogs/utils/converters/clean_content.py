00383d672dc31accf31c98990232f5aa92c2e4b0
Caleb Bartlett <coolum444@hotmail.co.uk>
2020-01-22 03:52:13 +0000

    Change CleanContent into a str subclass

--- cogs/utils/converters/clean_content.py
+++ cogs/utils/converters/clean_content.py
@@ -3,9 +3,10 @@ import re as regex
 from discord.ext import commands
 
 
-class CleanContent(commands.Converter):
+class CleanContent(str):
 
-    async def convert(self, argument:str):
+    @classmethod
+    async def convert(cls, ctx:commands.Context, argument:str):
         """Cleans up everyone and here mentions and leaves all else as is"""
 
         return regex.sub(r'@(everyone|here)', '@\u200b\\1', argument)
