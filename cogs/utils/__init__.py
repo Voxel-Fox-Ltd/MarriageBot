516d4d77ea6128582c20ad9238289964049c998d
Caleb Bartlett <coolum444@hotmail.co.uk>
2020-01-23 12:11:26 +0000

    Add time value util

--- cogs/utils/__init__.py
+++ cogs/utils/__init__.py
@@ -2,10 +2,11 @@ from cogs.utils.custom_command import CustomContext as Context
 from cogs.utils.custom_cog import CustomCog as Cog
 from cogs.utils.custom_bot import CustomBot as Bot
 from cogs.utils.context_embed import ContextEmbed as Embed
-
 from cogs.utils.custom_command import CustomCommand as Command, CustomGroup as Group
 from cogs.utils.database import DatabaseConnection
 
 from cogs.utils import checks
 from cogs.utils import converters
 from cogs.utils.checks import cooldown
+
+from cogs.utils.time_value import TimeValue
--- cogs/utils/errors.py
+++ cogs/utils/errors.py
@@ -1,1 +1,2 @@
 from cogs.utils.checks.is_config_set import ConfigNotSet
+from cogs.utils.time_value import InvalidTimeDuration
