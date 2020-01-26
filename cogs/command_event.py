afe6a8748b9695d95632c25dd7a8a85afee49d78
Caleb Bartlett <coolum444@hotmail.co.uk>
2020-01-26 01:57:18 +0000

    Fix guild/nonguild check being flipped whoops

--- cogs/command_event.py
+++ cogs/command_event.py
@@ -15,7 +15,7 @@ class CommandEvent(utils.Cog):
         content = ctx.message.content.replace('\n', '\\n')[:self.CONTENT_LIMIT]
         if len(ctx.message.content) > self.CONTENT_LIMIT:
             content += '...'
-        if ctx.guild:
+        if ctx.guild is None:
             return logger.info(f"Command invoked ({ctx.invoked_with}) ~ (G0/C{ctx.channel.id}/U{ctx.author.id}) :: {content}")
         logger.info(f"Command invoked ({ctx.invoked_with}) ~ (G{ctx.guild.id}/C{ctx.channel.id}/U{ctx.author.id}) :: {content}")
 
