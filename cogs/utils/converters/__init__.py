7b2a3110e3cf2cfa627af491954d223739b8ec4d
Caleb Bartlett <callum@voxelfox.co.uk>
2020-03-24 23:46:33 +0000

    Let the DM command work with non-cached users

--- cogs/utils/converters/__init__.py
+++ cogs/utils/converters/__init__.py
@@ -1,2 +1,3 @@
 # flake8: noqa
 from cogs.utils.converters.clean_content import CleanContent
+from cogs.utils.converters.user_id import UserID
---
+++ cogs/utils/converters/user_id.py
@@ -0,0 +1,25 @@
+import re as regex
+
+from discord.ext import commands
+
+
+class UserID(int):
+    """A conveter that takes the given value and tries to grab the ID from it
+    Returns the ID of the user"""
+
+    USER_ID_REGEX = regex.compile(r'([0-9]{15,21})')
+
+    @classmethod
+    async def convert(cls, ctx:commands.Context, value:str) -> int:
+        """Converts the given value to a valid user ID"""
+
+        # Maybe they gave a straight?
+        try:
+            return int(value)
+        except ValueError:
+            pass
+
+        # They pinged the user
+        match = cls.USER_ID_REGEX.search(value)
+        if match is not None:
+            return int(match.group(1))
