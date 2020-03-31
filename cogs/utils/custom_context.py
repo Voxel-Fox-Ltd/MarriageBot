36b82450c35563a0e012a6f60ae3d59cd0c6c596
Caleb Bartlett <callum@voxelfox.co.uk>
2020-03-31 21:45:43 +0100

    Add optional ignore_error arg to ctx.send

--- cogs/utils/custom_context.py
+++ cogs/utils/custom_context.py
@@ -7,3 +7,13 @@ class CustomContext(commands.Context):
         """Adds the okay hand reaction to a message"""
 
         return await self.message.add_reaction("\N{OK HAND SIGN}")
+
+    async def send(self, *args, ignore_error:bool=False, **kwargs):
+        """The normal ctx.send but with an optional arg to ignore errors"""
+
+        try:
+            return await super().send(*args, **kwargs)
+        except Exception as e:
+            if ignore_error:
+                return None
+            raise e
