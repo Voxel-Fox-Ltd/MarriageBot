8e8501b31bb01e13d1410f784b67f5196e91f90e
Caleb Bartlett <callum@voxelfox.co.uk>
2020-04-12 21:57:26 +0100

    Fix max family amount not being set properly

--- cogs/parentage.py
+++ cogs/parentage.py
@@ -53,7 +53,7 @@ class Parentage(utils.Cog):
             return await ctx.send(f"They're currently at the maximum amount of children you can have - see `m!perks` for more information.")
 
         # Check the size of their trees
-        max_family_members = self.bot.guild_settings[ctx.guild.id]['max_family_members']
+        max_family_members = self.bot.guild_settings[ctx.guild.id]['max_family_members'] if self.bot.is_server_specific else self.bot.config['max_family_members']
         async with ctx.channel.typing():
             if instigator_tree.family_member_count + target_tree.family_member_count > max_family_members:
                 return await ctx.send(f"If you added {target.mention} to your family, you'd have over {max_family_members} in your family, so I can't allow you to do that. Sorry!")
@@ -142,7 +142,7 @@ class Parentage(utils.Cog):
             return await ctx.send(f"You're currently at the maximum amount of children you can have - see `m!perks` for more information.")
 
         # Check the size of their trees
-        max_family_members = self.bot.guild_settings[ctx.guild.id]['max_family_members']
+        max_family_members = self.bot.guild_settings[ctx.guild.id]['max_family_members'] if self.bot.is_server_specific else self.bot.config['max_family_members']
         async with ctx.channel.typing():
             if instigator_tree.family_member_count + target_tree.family_member_count > max_family_members:
                 return await ctx.send(f"If you added {target.mention} to your family, you'd have over {max_family_members} in your family, so I can't allow you to do that. Sorry!")
