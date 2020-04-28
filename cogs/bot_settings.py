d7c3abbe791cd0069e60a138bc147955b014c43d
Caleb Bartlett <callum@voxelfox.co.uk>
2020-04-28 12:19:36 +0100

    Add basic setup command for bot base

--- cogs/bot_settings.py
+++ cogs/bot_settings.py
@@ -26,6 +26,26 @@ class BotSettings(utils.Cog):
                 await db("UPDATE guild_settings SET prefix=$2 WHERE guild_id=$1", ctx.guild.id, new_prefix)
         await ctx.send(f"My prefix has been updated to `{new_prefix}`.")
 
+    @commands.command(cls=utils.Command, enabled=False)
+    @commands.guild_only()
+    @commands.has_permissions(manage_guild=True)
+    @commands.bot_has_permissions(send_messages=True, embed_links=True)
+    async def setup(self, ctx:utils.Context):
+        """Run the bot setup"""
+
+        menu = utils.SettingsMenu()
+        settings_mention = utils.SettingsMenuOption.get_guild_settings_mention
+        menu.bulk_add_options(
+            ctx,
+            {
+                'display': lambda c: "Set setting (currently {0})".format(settings_mention(c, 'setting_id')),
+                'converter_args': [("What do you want to set the setting to?", "setting channel", commands.TextChannelConverter)],
+                'callback': utils.SettingsMenuOption.get_set_guild_settings_callback('setting_id'),
+            },
+        )
+        await menu.start(ctx)
+        await ctx.send("Done setting up!")
+
 
 def setup(bot:utils.Bot):
     x = BotSettings(bot)
