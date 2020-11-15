016a4a84f3e384a96f18e6814aed00637f4aad9a
Joshua Busacker <me@aiko.cc>
2020-11-16 09:45:10 +1100

    DM invite link and Leave server on unwhitelist

--- cogs/bot_moderator.py
+++ cogs/bot_moderator.py
@@ -98,6 +98,8 @@ class ModeratorOnly(utils.Cog):
 
         async with self.bot.database() as db:
             await db('INSERT INTO guild_specific_families (guild_id, purchased_by) VALUES ($1, $2) ON CONFLICT (guild_id) DO UPDATE SET purchased_by=excluded.purchased_by', guild_id, user_id)
+        user = self.bot.get_user(user_id) or await self.bot.fetch_user(user_id)
+        await user.send(f"Here's the link to invite MB Gold.\n<https://discordapp.com/oauth2/authorize?client_id=603608141434716171&scope=bot&permissions=49152>", embeddify=False)
         await ctx.okay(ignore_error=True)
 
     @commands.command(cls=utils.Command)
@@ -108,6 +110,8 @@ class ModeratorOnly(utils.Cog):
 
         async with self.bot.database() as db:
             await db('DELETE FROM guild_specific_families WHERE guild_id=$1', guild_id)
+        guild = bot.get_guild(guild_id) or await self.bot.fetch_guild(guild_id)
+        await guild.leave()
         await ctx.okay(ignore_error=True)
 
     @commands.command(cls=utils.Command, aliases=['getgoldpurchases'])
