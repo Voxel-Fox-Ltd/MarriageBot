32eb9bb975f95ab76ba7272b52f9ad9b3dd228f8
Kae Bartlett <Kae@voxelfox.co.uk>
2021-02-27 08:02:20 +0000

    Revamp the shallow user system

---
+++ cogs/utils/discord_name_manager.py
@@ -0,0 +1,77 @@
+import typing
+
+import voxelbotutils
+
+
+class DiscordNameManager(object):
+
+    cached_names: typing.Dict[int, 'DiscordNameManager'] = {}
+
+    __slots__ = ("user_id", "name", "age",)
+
+    def __init__(self, user_id:int, name:str=None):
+        self.user_id: int = user_id
+        self._name: str = name
+        self.age: int = 0
+        self.cached_names[self.user_id] = self
+
+    @property
+    def name(self):
+        self.age += 1
+        return self._name
+
+    @name.setter
+    def name(self, new_name:str):
+        self.age = 0
+        self._name = new_name
+
+    @property
+    def name_is_valid(self):
+        return self.age <= 3
+
+    async def fetch_name(self, bot:voxelbotutils.Bot) -> str:
+        """
+        Fetch the name of the current user - first trying from Redis, then trying from the
+        API (if we couldn't get from Redis the first time then we add it after fetching from
+        the API).
+        """
+
+        async with bot.redis() as re:
+            v = await re.get(f"UserName-{self.user_id}")
+        if v:
+            self.name = v
+            return v
+        try:
+            user = await bot.fetch_user(self.user_id)
+            name = str(user)
+        except Exception:
+            name = "Deleted User"
+        async with bot.redis() as re:
+            await re.set(f"UserName-{self.user_id}", name)
+        self.name = name
+        return name
+
+    @classmethod
+    async def get_name(cls, bot:voxelbotutils.Bot, user_id:int) -> str:
+        """
+        Get the name for a user given their ID.
+
+        Args:
+            bot (voxelbotutils.Bot): The bot instance that we can use to fetch from the API/Redis with.
+            user_id (int): The ID of the user we want to grab the name of.
+
+        Returns:
+            str: The user's name.
+        """
+
+        # Grab our cached object
+        v = cls.cached_names.get(user_id)
+        if v is None:
+            v = cls(user_id)
+
+        # See if it has a name
+        if v.name_is_valid:
+            return v.name
+
+        # Grab a new name from the cache for them
+        return v.fetch_name(bot)
