bda6ee1105e6d510148b728965915c11e9ee2285
Kae Bartlett <Kae@voxelfox.co.uk>
2021-02-27 08:02:10 +0000

    Organise imports

--- cogs/utils/__init__.py
+++ cogs/utils/__init__.py
@@ -1,20 +1,6 @@
-# flake8: noqa
 from cogs.utils import checks, converters, errors, random_text
 from cogs.utils.acceptance_check import AcceptanceCheck
-from cogs.utils.checks import cooldown
-from cogs.utils.context_embed import ContextEmbed as Embed
-from cogs.utils.custom_bot import CustomBot as Bot
-from cogs.utils.custom_cog import CustomCog as Cog
-from cogs.utils.custom_command import CustomCommand as Command
-from cogs.utils.custom_command import CustomGroup as Group
-from cogs.utils.custom_context import CustomContext as Context
 from cogs.utils.customised_tree_user import CustomisedTreeUser
-from cogs.utils.database import DatabaseConnection
 from cogs.utils.family_tree.family_tree_member import FamilyTreeMember
 from cogs.utils.family_tree.relation_simplifier import Simplifier
 from cogs.utils.proposal_cache import ProposalCache
-from cogs.utils.redis import RedisConnection
-from cogs.utils.shallow_user import ShallowUser
-from cogs.utils.time_value import TimeValue
-from cogs.utils.settings_menu import SettingsMenu, SettingsMenuOption, SettingsMenuIterable
-from cogs.utils.weeb_sh import get_reaction_gif
--- cogs/utils/errors.py
+++ cogs/utils/errors.py
@@ -1,11 +1,3 @@
-# flake8: noqa
-from cogs.utils.checks.bot_is_ready import BotNotReady
-from cogs.utils.checks.is_bot_moderator import NotBotSupport, NotBotModerator  # NOQA
-from cogs.utils.checks.has_donator_perks import IsNotDonator  # NOQA
+from cogs.utils.checks.is_bot_moderator import NotServerSpecificBotModerator
 from cogs.utils.checks.is_server_specific import NotServerSpecific
-from cogs.utils.checks.is_voter import IsNotVoter
-from cogs.utils.checks.meta_command import InvokedMetaCommand
 from cogs.utils.converters.user_block import BlockedUserError
-from cogs.utils.missing_required_argument import MissingRequiredArgumentString
-from cogs.utils.time_value import InvalidTimeDuration
-from cogs.utils.checks.is_config_set import ConfigNotSet
