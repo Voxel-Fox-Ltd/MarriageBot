0bbcad586bf42e33a5d18eefb238c05f0aebb5c4
fadedmax <me@fadedmax.net>
2021-03-15 20:10:09 -0600

    Change ship file locations, and some bot support cog changes.

--- cogs/misc_commands.py
+++ cogs/misc_commands.py
@@ -15,49 +15,6 @@ class MiscCommands(utils.Cog):
         self.process = psutil.Process(os.getpid())
         self.process.cpu_percent()
 
-    @commands.command(cls=utils.Command)
-    @commands.bot_has_permissions(send_messages=True)
-    async def ship(self, ctx:utils.Context, user:discord.Member, user2:discord.Member=None):
-        """
-        Gives you a ship percentage between two users.
-        """
-
-        # Fix attrs
-        if user2 is None:
-            user, user2 = ctx.author, user
-
-        # Add response for yourself
-        if user == user2:
-            return await ctx.send("-.-")
-
-        # Get percentage
-        async with self.bot.database() as db:
-            rows = await db("SELECT * FROM ship_percentages WHERE user_id_1=ANY($1::BIGINT[]) AND user_id_2=ANY($1::BIGINT[])", [user.id, user2.id])
-        if rows and rows[0]['percentage']:
-            percentage = rows[0]['percentage'] / 100
-        else:
-            percentage = ((user.id + user2.id + 4500) % 10001) / 100
-        return await ctx.send(f"{user.mention} \N{REVOLVING HEARTS} **{percentage:.2f}%** \N{REVOLVING HEARTS} {user2.mention}", allowed_mentions=discord.AllowedMentions(users=False))
-
-    @utils.command(hidden=True)
-    @utils.checks.is_bot_support()
-    @commands.bot_has_permissions(add_reactions=True)
-    async def addship(self, ctx:utils.Context, user1:discord.Member, user2:typing.Optional[discord.Member]=None, percentage:float=0):
-        """
-        Add a custom ship percentage.
-        """
-
-        user2 = user2 or ctx.author
-        percentage = max([min([percentage * 100, 10_000]), -10_000])
-        async with self.bot.database() as db:
-            await db(
-                """INSERT INTO ship_percentages (user_id_1, user_id_2, percentage) VALUES ($1, $2, $3)
-                ON CONFLICT (user_id_1, user_id_2) DO UPDATE SET percentage=excluded.percentage""",
-                *sorted([user1.id, user2.id]), percentage,
-            )
-        await ctx.okay()
-
-
     @commands.command(aliases=['upvote'], cls=utils.Command)
     @utils.cooldown.cooldown(1, 5, commands.BucketType.user)
     @commands.bot_has_permissions(send_messages=True)
