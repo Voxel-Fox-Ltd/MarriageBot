637f9f5de311e0f982c7784157e7f46ee03e4b7c
Kae Bartlett <Kae@voxelfox.co.uk>
2021-04-04 22:44:05 +0100

    Raise an error earlier because it won't work apparently

--- cogs/utils/checks/is_bot_moderator.py
+++ cogs/utils/checks/is_bot_moderator.py
@@ -19,13 +19,22 @@ def is_server_specific_bot_moderator():
     """
 
     async def predicate(ctx:utils.Context):
+        # Bot support can do anything
         try:
             await utils.checks.is_bot_support().predicate(ctx)
             return True
         except Exception:
             pass
-        await guild_is_server_specific().predicate(ctx)
+
+        # Make sure we're on Gold
+        try:
+            await guild_is_server_specific().predicate(ctx)
+        except Exception:
+            raise commands.NotServerSpecificBotModerator()
+
+        # Make sure they have the role
         if any([i for i in ctx.author.roles if i.name.casefold() in 'marriagebot moderator']):
             return True
         raise commands.NotServerSpecificBotModerator()
+
     return commands.check(predicate)
