b3791d6502e4764cdaee76520bc51a085cd3a212
Kae Bartlett <Kae@voxelfox.co.uk>
2021-04-04 15:31:57 +0100

    Fix blocked user commands

--- cogs/block_commands.py
+++ cogs/block_commands.py
@@ -7,24 +7,20 @@ class BlockCommands(utils.Cog):
 
     @utils.command()
     @commands.bot_has_permissions(send_messages=True)
-    async def block(self, ctx:utils.Context, user_id:utils.converters.UserID):
+    async def block(self, ctx:utils.Context, user:utils.converters.UserID):
         """
         Blocks a user from being able to adopt/makeparent/whatever you.
         """
 
-        # Get current blocks
-        current_blocks = self.bot.blocked_users.get(ctx.author.id, list())
-        if user_id in current_blocks:
-            return await ctx.send("That user is already blocked.")
-
         # Add to list
         async with self.bot.database() as db:
             await db(
-                'INSERT INTO blocked_user (user_id, blocked_user_id) VALUES ($1, $2)',
-                ctx.author.id, user_id
+                """INSERT INTO blocked_user (user_id, blocked_user_id) VALUES ($1, $2)
+                ON CONFLICT (user_id, blocked_user_id) DO NOTHING""",
+                ctx.author.id, user,
             )
         async with self.bot.redis() as re:
-            await re.publish("BlockedUserAdd", {"user_id": ctx.author.id, "blocked_user_id": user_id})
+            await re.publish("BlockedUserAdd", {"user_id": ctx.author.id, "blocked_user_id": user})
         return await ctx.send("That user is now blocked.")
 
     @utils.command()
@@ -34,16 +30,11 @@ class BlockCommands(utils.Cog):
         Unblocks a user and allows them to adopt/makeparent/whatever you.
         """
 
-        # Get current blocks
-        current_blocks = self.bot.blocked_users[ctx.author.id]
-        if user not in current_blocks:
-            return await ctx.send("You don't have that user blocked.")
-
         # Remove from list
         async with self.bot.database() as db:
             await db(
-                'DELETE FROM blocked_user WHERE user_id=$1 AND blocked_user_id=$2',
-                ctx.author.id, user
+                """DELETE FROM blocked_user WHERE user_id=$1 AND blocked_user_id=$2""",
+                ctx.author.id, user,
             )
         async with self.bot.redis() as re:
             await re.publish("BlockedUserRemove", {"user_id": ctx.author.id, "blocked_user_id": user})
