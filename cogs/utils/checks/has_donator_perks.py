40b3bfc8fc0eafb8ca0bbbefff08357bbf291ff3
fadedmax <me@fadedmax.net>
2021-04-06 21:33:11 -0600

    Remove broken bot support check.

--- cogs/utils/checks/has_donator_perks.py
+++ cogs/utils/checks/has_donator_perks.py
@@ -6,11 +6,6 @@ from cogs.utils.perks_handler import get_marriagebot_perks
 
 def has_donator_perks(perk_name:str):
     async def predicate(ctx):
-    	bot_support = await utils.checks.is_bot_support().predicate(ctx)
-    	if bot_support:
-    		return True
-    	else:
-    		pass
         perks = await get_marriagebot_perks(ctx.bot, ctx.author.id)
         v = getattr(perks, perk_name, False)
         if v:
