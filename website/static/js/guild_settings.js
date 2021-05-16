86eb2afb7fcee9ad250d43e0964483585e457552
Kae Bartlett <Kae@voxelfox.co.uk>
2021-05-16 23:52:43 +0100

    Add the starts of our guild settings post JS file

---
+++ website/static/js/guild_settings.js
@@ -0,0 +1,22 @@
+async function updateGuildPrefix(submitButton) {
+    // Get the form node
+    let form = submitButton;
+    while (form.nodeName !== "FORM") {
+        form = form.parentNode;
+    }
+
+    // Send the web request
+    response = await fetch("/set_prefix", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(Object.fromEntries(URLSearchParams(form.serialize()))),
+    });
+    if(response.ok) {
+        alert("Yo it worked sick");
+    }
+    else {
+        alert("AAAAAAA ERROR");
+    }
+}
