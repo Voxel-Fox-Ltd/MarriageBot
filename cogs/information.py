9784887254af7abe07e2e2e86f770920cc8c9026
George M <32426140+iHeroGH@users.noreply.github.com>
2021-06-12 08:09:39 -0700

    Fixed siblings command: user.id can not exist, user_id always does

--- cogs/information.py
+++ cogs/information.py
@@ -138,7 +138,7 @@ class Information(utils.Cog):
             output = f"**{localutils.escape_markdown(user_name)}** has no siblings right now." if user_id != ctx.author.id else "You have no siblings right now."
         else:
             # Remove the user from the sibling list
-            sibling_list = [sibling for sibling in sibling_list if sibling != user.id]
+            sibling_list = [sibling for sibling in sibling_list if sibling != user_id]
             siblings_plural = 'sibling' if len(sibling_list) == 1 else 'siblings'
             # Count the siblings
             output = f"**{localutils.escape_markdown(user_name)}** has {len(sibling_list)} {sibling_plural}:\n"
