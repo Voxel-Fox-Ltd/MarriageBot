782fe2982b7930b81b5034d2c3a9b9dab1a54e3e
Kae Bartlett <kae@voxelfox.co.uk>
2021-07-16 05:59:01 +0100

    make our rows pickleable

--- cogs/cache_handler.py
+++ cogs/cache_handler.py
@@ -52,12 +52,12 @@ class CacheHandler(vbu.Cog):
             # Cache the family data - partners
             self.logger.info(f"Caching {len(partnerships)} partnerships from partnerships")
             for i in partnerships:
-                await self.bot.loop.run_in_executor(executor, functools.partial(self.handle_partner, i))
+                await self.bot.loop.run_in_executor(executor, functools.partial(self.handle_partner, dict(i)))
 
             # - children
             self.logger.info(f"Caching {len(parents)} parents/children from parents")
             for i in parents:
-                await self.bot.loop.run_in_executor(executor, functools.partial(self.handle_parent, i))
+                await self.bot.loop.run_in_executor(executor, functools.partial(self.handle_parent, dict(i)))
 
         # And done
         return True
