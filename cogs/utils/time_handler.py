83e1b1ba68305be6d5e958908e6b6f9fc2987f31
George M <32426140+iHeroGH@users.noreply.github.com>
2021-07-18 19:34:07 -0700

    Added the time_handler util file

---
+++ cogs/utils/time_handler.py
@@ -0,0 +1,17 @@
+from datetime import datetime as dt
+import re
+
+
+def fix_time_string(time_str:str):
+  """
+  Takes a string of Day/Month(/Year) and returns a tuple of (day, month, year/current year)
+  """
+
+  # Set up the Regex statement
+  time_match = re.search(r"^(?P<day>\d+)\/(?P<month>\d+)(\/(?P<year>\d+))?$", time_str)
+
+  day = int(time_match.group("day"))
+  month = int(time_match.group("month"))
+  year = int(time_match.group("year") or dt.utcnow().year)
+
+  return dt(year, month, day)
