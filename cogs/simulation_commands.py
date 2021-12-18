d1b3c891d58603f2e1bba267bf079c9967373e27
Kae Bartlett <kae@voxelfox.co.uk>
2021-12-18 11:04:06 +0000

    Fix missing followup attr

--- cogs/simulation_commands.py
+++ cogs/simulation_commands.py
@@ -528,7 +528,7 @@ class SimulationCommands(vbu.Cog):
             return
 
         # Respond
-        await result.ctx.send(
+        await result.ctx.followup.send(
             random.choice(utils.random_text.Copulate.VALID).format(author=ctx.author, target=target),
             wait=False,
         )
