43bf0270ee5393a36bfc142f4c3528ecbc49772f
Kae Bartlett <kae@voxelfox.co.uk>
2022-08-03 21:48:52 +0100

    Update PM2 file

---
+++ pm2.js
@@ -0,0 +1,42 @@
+function generate(name, script) {
+    return {
+        name: name,
+        autorestart: false,
+        script: script,
+        cwd: "/home/kae/MarriageBot",
+    }
+}
+
+
+const SHARD_COUNT = 720;
+const CLUSTER_COUNT = 10;
+
+
+function generateStatus(index) {
+    let min = index * (SHARD_COUNT / CLUSTER_COUNT);
+    let max = ((index + 1) * (SHARD_COUNT / CLUSTER_COUNT)) - 1;
+    return generate(
+        `status${index}`,
+        `.venv/bin/vbu run-bot --no-startup --min ${min} --max ${max} --shardcount ${SHARD_COUNT}`,
+    )
+}
+
+
+let statusApps = [];
+for(let i of Array(CLUSTER_COUNT).keys()) {
+    statusApps.push(generateStatus(i))
+}
+
+
+module.exports = {
+    apps: [
+        generate("mb", ".venv/bin/vbu run-interactions --port 8001 --path /"),
+        ...statusApps,
+        generate("gold", ".venv/bin/vbu run-bot . config/gold.toml --shardcount 1"),
+        generate("web", ".venv/bin/vbu run-website --port 8000"),
+        generate("sharder", ".venv/bin/vbu run-sharder --loglevel debug --port 8888 --concurrency 16"),
+    ]
+}
+
+
+console.log(module.exports.apps);
