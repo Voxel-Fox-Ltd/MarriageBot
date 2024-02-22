24ba85287fc89b0655fd2ab8e7635ee1a85660f8
Kae Bartlett <kae@voxelfox.co.uk>
2024-02-22 02:18:45 +0000

    Add divorce command

--- plugins/marriage.py
+++ plugins/marriage.py
@@ -71,4 +71,73 @@ class Marriage(client.Plugin):
         Divorce one of your partners.
         """
 
-        ...
+        guild_id: int = ctx.guild.id if self.bot.config.gold else 0
+        fm = u.FamilyMember.get(ctx.user.id, guild_id)
+        async with db.Database.acquire() as conn:
+            names = await u.get_names(conn, *fm._partner_ids)
+
+        if not names:
+            return await ctx.send(
+                ctx._("You don't have any partners right now!"),
+                ephemeral=True,
+            )
+
+        return await ctx.send(
+            ctx._("Which of your partners do you want to divorce?"),
+            components=[
+                n.ActionRow([
+                    n.StringSelectMenu(
+                        custom_id=f"DIVORCE {ctx.user.id}",
+                        options=[
+                            n.SelectOption(label=o, value=str(i))
+                            for i, o in names.items()
+                        ],
+                    ),
+                ]),
+            ],
+        )
+
+    @client.event.filtered_component(r"DIVORCE \d+")
+    async def divorce_dropdown_clicked(self, ctx: t.ComponentGI) -> None:
+        """
+        Pinged when a divorce button is clicked.
+        """
+
+        # Make sure that the user who clicked the button is the same user
+        # that the button was made for
+        # It SHOULD be because the message is ephemeral, but we should check
+        _, required_user_id_str = ctx.data.custom_id.split(" ")
+        required_user_id = int(required_user_id_str)
+        if required_user_id != ctx.user.id:
+            await ctx.send(
+                ctx._("You cannot interact with these buttons."),
+                ephemeral=True,
+            )
+            return
+
+        # Divorce them from whomever they clicked on
+        clicked_user_str = ctx.data.values[0].value
+        clicked_user = int(clicked_user_str)
+        ft = u.FamilyMember.get(ctx.user.id)
+        probable_success = clicked_user in ft._partner_ids
+        async with db.Database.acquire() as conn:
+            await ft.db.remove_partner(conn, u.FamilyMember.get(clicked_user))
+
+        # And done
+        if probable_success:
+            await ctx.update(
+                content=(
+                    ctx._("You have been divorced from {user} :(")
+                    .format(user=f"<@{clicked_user}>")
+                ),
+                components=None,
+            )
+            return
+        await ctx.update(
+            content=(
+                ctx._("You have been divorced from {user} :(")
+                .format(user=f"<@{clicked_user}>")
+            ),
+            components=None,
+        )
+        return
