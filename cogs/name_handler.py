af79a4c93485f2b7f7d7de27e3ed80fc4d4811b2
Kae Bartlett <kae@voxelfox.co.uk>
2024-07-02 14:33:48 +0100

    Add command to forcibly update usernames inside of trees

--- cogs/name_handler.py
+++ cogs/name_handler.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Union
+from typing import Optional, Union
 
 import discord
 from discord.ext import commands, vbu
@@ -22,6 +22,26 @@ class NameHandler(vbu.Cog):
     @vbu.Cog.listener()
     async def on_command(self, ctx: commands.Context):
         return await self.save_name(ctx.author)
+
+    @commands.command(
+        application_command_meta=commands.ApplicationCommandMeta(
+            options=[
+                discord.ApplicationCommandOption(
+                    name="user",
+                    description="The user whose username you want to update.",
+                    type=discord.ApplicationCommandOptionType.user,
+                    required=False,
+                ),
+            ],
+        ),
+    )
+    async def updatename(self, ctx: commands.Context, user: Optional[discord.User] = None):
+        """
+        Update a saved name inside of MarriageBot.
+        """
+
+        await self.save_name(user or ctx.author)
+        await ctx.send("Updated :)")
 
 
 def setup(bot: vbu.Bot):
