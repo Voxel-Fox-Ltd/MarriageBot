31311bd74fbc4ccf30474cd8d6bb1a05a7422b5e
Kae Bartlett <kae@voxelfox.co.uk>
2025-07-07 19:11:29 +0100

    Enable tree command for the support guild

--- plugins/information.py
+++ plugins/information.py
@@ -387,8 +387,9 @@ class Information(client.Plugin):
         Handles the generation and sending of the tree to the user.
         """
 
-        await ctx.send("This command has been temporarily disabled.")
-        return
+        if (ctx.guild or ctx.user).id != 208895639164026880:
+            await ctx.send("This command has been temporarily disabled.")
+            return
 
         # Get their family tree
         guild_id: int = await u.get_guild_id(self.bot, ctx)
